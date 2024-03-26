478064b48cd40e458780eec90795782a78d54918
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-03-26 22:38:27 +0000

    feat: introduced abstract base classes for receivers

---
+++ spectre/receivers/CaptureConfigMount.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+
+class CaptureConfigMount(ABC):
+    def __init__(self):
+        self.set_templates()
+        self.set_validators()
+        
+        # we impose that for any config detailed in templates, it must have a corresponding validator
+        if self.templates.keys() != self.validators.keys():
+            raise KeyError(f"Key mismatch between templates and validators for {self.receiver_name}!")
+        self.valid_modes = list(self.templates.keys())
+    
+
+    @abstractmethod
+    def set_validators(self):
+        pass
+
+
+    @abstractmethod
+    def set_templates(self):
+        pass
+
+
+    def validate(self, capture_config, mode):
+        validator = self.get_validator(mode)
+        validator(capture_config)
+
+
+    def get_validator(self, mode):
+        return self.validators[mode]
+    
+
+    def get_template(self, mode):
+        return self.templates[mode]
+    
+
+
+
+
+
+    
+
+
+
