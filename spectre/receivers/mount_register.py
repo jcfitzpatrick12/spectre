f695d48bc36f8647f2c1c63fd70013598e37f6aa
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-03-27 19:39:58 +0000

    feat: class decorators to register mount classes automatically

--- spectre/receivers/mount_register.py
+++ spectre/receivers/mount_register.py
@@ -17,18 +17,3 @@ def register_capture_mount(receiver_name):
         capture_mounts[receiver_name] = cls
         return cls
     return decorator
-
-
-# fetch a capture config mount for a given receiver name
-def get_capture_config_mount(receiver_name):
-    if receiver_name in capture_config_mounts:
-        return capture_config_mounts[receiver_name]()
-    else:
-        raise ValueError(f"Capture config for {receiver_name} not found.")
-
-# fetch a capture mount for a given receiver name
-def get_capture_mount(receiver_name):
-    if receiver_name in capture_mounts:
-        return capture_mounts[receiver_name]()
-    else:
-        raise ValueError(f"Capture mount for {receiver_name} not found.")
