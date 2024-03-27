d4050efefb16e772ac202978bd562d5da4955169
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-03-27 20:10:22 +0000

    feat: improved error handling and readability

--- spectre/receivers/get_mount.py
+++ spectre/receivers/get_mount.py
@@ -6,16 +6,27 @@ from spectre.receivers.mount_register import capture_config_mounts, capture_moun
 
 # fetch a capture config mount for a given receiver name
 def get_capture_config_mount(receiver_name: str):
-    valid_receivers = list(capture_config_mounts.keys())
-    if not receiver_name in valid_receivers:
+    # try and fetch the capture config mount
+    capture_config_mount = capture_config_mounts.get(receiver_name)
+    if capture_config_mount is None:
+        valid_receivers = list(capture_config_mounts.keys())
         raise ValueError(f"No capture config mount found for receiver: {receiver_name}. Please specify one of the following receivers {valid_receivers}.")
-    return capture_config_mounts[receiver_name]()
+    # if found, return an instance of the class
+    try:
+        return capture_config_mount()
+    except TypeError as e:
+        raise TypeError(f"Failed to create instance of capture config mount for receiver {receiver_name}. Received the error: {e}")
 
 
 # fetch a capture mount for a given receiver name
 def get_capture_mount(receiver_name: str):
-    valid_receivers = list(capture_mounts.keys())
-    if not receiver_name in valid_receivers:
+    # try and fetch the capture config mount
+    capture_mount = capture_mounts.get(receiver_name)
+    if capture_mount is None:
+        valid_receivers = list(capture_config_mounts.keys())
         raise ValueError(f"No capture mount found for receiver: {receiver_name}. Please specify one of the following receivers {valid_receivers}.")
-
-    return capture_mounts[receiver_name]()
+    # if found, return an instance of the class
+    try:
+        return capture_mount()
+    except TypeError as e:
+            raise TypeError(f"Failed to create instance of capture mount for receiver {receiver_name}. Received the error: {e}")
