3e4354e05e02e1f6806c2b64d7c7f213a77d4d76
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-03-29 13:52:45 +0000

    feat: check gr is a directory not a file and other minor changes

--- spectre/receivers/library/__init__.py
+++ spectre/receivers/library/__init__.py
@@ -8,12 +8,10 @@ import pkgutil
 # which is decorated to the global list of mounts
 # Iterate through all receiver directories within the library
 library_path = os.path.dirname(__file__)
-
 expected_modules_for_receiver = ['Capture', 'CaptureConfig']
 
 def import_from_receiver(receiver):
-
-    # creat the full path to the receiver, 
+    # create the full path to the receiver, 
     receiver_path = os.path.join(library_path, receiver)
     # ensure this path describes a directory, not a file
     if not os.path.isdir(receiver_path):
@@ -23,17 +21,22 @@ def import_from_receiver(receiver):
     dir_contents = [d for d in os.listdir(receiver_path) if not d == "__pycache__"]
 
     if "gr" not in dir_contents:
-        raise NotADirectoryError(f"Could not find gr folder for receiver {receiver}.")
+        raise NotADirectoryError(f"Could not find \"gr\" directory for receiver {receiver}.")
+
+    gr_path = os.path.join(receiver_path, 'gr')
+
+    if not os.path.isdir(gr_path):
+        raise NotADirectoryError(f"{gr_path} is not a directory. \"gr\" must be a specified as a directory.")
     
 
     if "mounts" not in dir_contents:
-        raise NotADirectoryError(f"Could not find mounts folder for receiver {receiver}.")
+        raise NotADirectoryError(f"Could not find \"mounts\" directory for receiver {receiver}.")
     
     
     mounts_path = os.path.join(receiver_path, 'mounts')
 
     if not os.path.isdir(mounts_path):
-        raise NotADirectoryError(f"{mounts_path} is not a directory.")
+        raise NotADirectoryError(f"{mounts_path} is not a directory. \"mounts\" must be a specified as a directory.")
     
     # Import all modules in the receiver's 'mounts' subdirectory
     for _, module_name, _ in pkgutil.iter_modules([mounts_path]):
