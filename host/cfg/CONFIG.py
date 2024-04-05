73511490008ec7be12f54c0b7b82cefa7d369e88
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-05 22:28:24 +0000

    chore: include path to data in CONFIG

--- host/cfg/CONFIG.py
+++ host/cfg/CONFIG.py
@@ -3,3 +3,4 @@ import os
 path_to_capture_configs = os.path.join(os.environ['SPECTREPARENTPATH'],"host", "capture_configs")
 path_to_PID = os.path.join('/home', 'spectre', 'host', 'cfg', 'process.pid')
 path_to_capture_log = os.path.join('/home', 'spectre', 'host', 'cfg', 'capture.log')
+path_to_data = os.path.join('/home', 'spectre', 'host', 'data')
