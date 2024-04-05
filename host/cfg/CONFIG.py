95bee1a09009af73167cc6d0fa307b020e75b1f4
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-05 22:09:13 +0000

    feat: add config paths for PID capture and capture log

--- host/cfg/CONFIG.py
+++ host/cfg/CONFIG.py
@@ -1,3 +1,5 @@
 import os
 
 path_to_capture_configs = os.path.join(os.environ['SPECTREPARENTPATH'],"host", "capture_configs")
+path_to_PID = os.path.join('/home', 'spectre', 'host', 'cfg', 'process.pid')
+path_to_capture_log = os.path.join('/home', 'spectre', 'host', 'cfg', 'capture.log')
