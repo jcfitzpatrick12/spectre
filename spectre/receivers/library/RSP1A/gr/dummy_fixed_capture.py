156c6965ea79280a4b216ce1326df1133b3f3081
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-05 22:13:04 +0000

    chore: removed redundant scripts

--- spectre/receivers/library/RSP1A/gr/dummy_fixed_capture.py
+++
@@ -1,6 +0,0 @@
-from spectre.utils import json_helpers
-
-def main(capture_config_dict: dict) -> None:
-    print("Dummy fixed capture using capture config:\n")
-    json_helpers.print_config(capture_config_dict)
-    return 
