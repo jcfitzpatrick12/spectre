6e7f7497790c482355ad8e0c9819ea7ae7b5f350
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-06 18:39:44 +0000

    feat: update paths

--- host/cfg/CONFIG.py
+++ host/cfg/CONFIG.py
@@ -1,6 +1,6 @@
 import os
 
 path_to_capture_configs = os.path.join(os.environ['SPECTREPARENTPATH'],"host", "capture_configs")
-path_to_PID = os.path.join('/home', 'spectre', 'host', 'cfg', 'process.pid')
 path_to_capture_log = os.path.join('/home', 'spectre', 'host', 'cfg', 'capture.log')
 path_to_data = os.path.join('/home', 'spectre', 'host', 'data')
+path_to_start_capture = os.path.join('/home', 'spectre', 'host', 'utils', 'start_capture.py')
