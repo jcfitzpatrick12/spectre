3966215f12446cca2f3490f35e5eafb3d1dc7689
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-07 19:07:45 +0000

    feat: generalise for more json_configs in the future

--- host/cli/tools/list/commands.py
+++ host/cli/tools/list/commands.py
@@ -9,27 +9,28 @@ app = typer.Typer()
 @app.command()
 def capture_configs(
 ) -> None:
-    capture_config_files = os.listdir(CONFIG.path_to_capture_configs)
-    typer.secho(f'Listing all capture configs in the directory: {CONFIG.path_to_capture_configs}')
-    for capture_config_file in capture_config_files:
-        typer.secho(
-            f'-> {capture_config_file}',
-        )
+    json_config_files = os.listdir(CONFIG.json_configs_dir)
+    typer.secho(f'Listing all capture configs in the directory: {CONFIG.json_configs_dir}')
+    for json_config_file in json_config_files:
+        if json_config_file.startswith("capture_config"):
+            typer.secho(
+                f'-> {json_config_file}',
+            )
     raise typer.Exit(1)
 
 
 @app.command()
 def tags(
 ) -> None:
-    
-    capture_config_files = os.listdir(CONFIG.path_to_capture_configs)
-
     # if len(capture_config_files) == 0:
     typer.secho(f'Listing all defined tags.')
-    for capture_config_file in capture_config_files:
-        config_file_name = os.path.splitext(capture_config_file)[0]
-        tag = "_".join(config_file_name.split("_")[2:])
-        typer.secho(
-            f'-> {tag}',
-        )
+    json_config_files = os.listdir(CONFIG.json_configs_dir)
+    for json_config_file in json_config_files:
+        if json_config_file.startswith("capture_config"):
+            config_file_name = os.path.splitext(json_config_file)[0]
+            tag = "_".join(config_file_name.split("_")[2:])
+            typer.secho(
+                f'-> {tag}',
+            )
     raise typer.Exit(1)
+
