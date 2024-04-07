46667a0886b0afb15e01e8d64ab2401d7bc3b6b2
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-07 19:05:10 +0000

    feat: update CONFIG with more reflective variable names

--- host/cfg/CONFIG.py
+++ host/cfg/CONFIG.py
@@ -1,6 +1,7 @@
 import os
 
-path_to_capture_configs = os.path.join(os.environ['SPECTREPARENTPATH'],"host", "capture_configs")
-path_to_capture_log = os.path.join('/home', 'spectre', 'host', 'cfg', 'capture.log')
-path_to_data = os.path.join('/home', 'spectre', 'host', 'data')
-path_to_start_capture = os.path.join('/home', 'spectre', 'host', 'utils', 'start_capture.py')
+json_configs_dir = os.path.join(os.environ['SPECTREPARENTPATH'],"host", "cfg", "json_configs")
+chunks_dir = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'chunks')
+path_to_capture_log = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'cfg', 'capture.log')
+path_to_start_capture = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'utils', 'start_capture.py')
+path_to_post_proc = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'utils', 'post_proc.py')
