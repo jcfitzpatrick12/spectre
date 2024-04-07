8ec35cbdbad1c2418d1f0f183c6bd05f7c4f04a0
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-07 19:06:28 +0000

    chore: error-handling and changed variable names

--- host/cli/tools/delete/commands.py
+++ host/cli/tools/delete/commands.py
@@ -4,8 +4,9 @@ import os
 from cli import __app_name__, __version__
 from cfg import CONFIG
 
-from spectre.capture_config.CaptureConfig import CaptureConfig
-from spectre.utils import json_helpers 
+from spectre.cfg.json_config.CaptureConfig import CaptureConfig
+# from spectre.cfg.json_config.TagMap import TagMap
+from spectre.utils import json_helpers
 
 app = typer.Typer()
 
@@ -19,15 +20,20 @@ def capture_config(tag: str = typer.Option(None, "--tag", "-t", help=""),
             fg=typer.colors.RED,
         )
         raise typer.Exit(1)
-    
-    capture_config = CaptureConfig(tag, CONFIG.path_to_capture_configs)
-    config_path = capture_config.absolute_path()
+
+    try:
+        capture_config = CaptureConfig(tag, CONFIG.json_configs_dir)
+        config_path = capture_config.absolute_path()
+
+    except TypeError as e:
+        raise TypeError(f"Could not instantiate capture config. Received: {e}")
 
     if not os.path.exists(config_path):
         typer.secho(
             f'Could not delete capture config with tag {tag}. {config_path} does not exist.',
             fg=typer.colors.RED,
         )
+        raise typer.Exit(1)
     
     else:
         json_helpers.doublecheck_delete(config_path)
@@ -37,5 +43,8 @@ def capture_config(tag: str = typer.Option(None, "--tag", "-t", help=""),
             fg=typer.colors.YELLOW,
         )
 
+    
     raise typer.Exit(1)
+
+
 
