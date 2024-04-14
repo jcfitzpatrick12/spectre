095865a76f2ae1059c5e3e625ddc22db3acd68e1
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-14 12:14:39 +0000

    fix: path issue following refactoring

--- host/cfg/CONFIG.py
+++ host/cfg/CONFIG.py
@@ -3,5 +3,5 @@ import os
 json_configs_dir = os.path.join(os.environ['SPECTREPARENTPATH'],"host", "cfg", "json_configs")
 chunks_dir = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'chunks')
 path_to_capture_log = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'cfg', 'capture.log')
-path_to_start_capture = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'utils', 'start_capture.py')
+path_to_start_capture = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'capture_scripts', 'start_capture.py')
 path_to_post_proc = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'utils', 'post_proc.py')
