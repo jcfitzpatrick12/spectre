d11ab0e137928c3697f4a55f63b3bdaa1d6bc9ae
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-14 16:19:57 +0000

    feat: new module for directory functions

---
+++ spectre/utils/dir_helpers.py
@@ -0,0 +1,19 @@
+import os
+import warnings
+
+
+def list_all_files(parent_dir: str):
+        if not os.path.exists(parent_dir):
+            raise NotADirectoryError(f"Could not find {parent_dir}.")
+        
+        all_files = []
+        #walk through all the subdirectories in data
+        for root, dirs, files in os.walk(parent_dir):
+            for file in files:
+                all_files.append(file)
+        
+        if len(all_files) == 0:
+            warnings.warn(f"No files identified within {parent_dir}", UserWarning)
+            
+        return all_files
+
