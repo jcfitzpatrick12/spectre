976228de79ad61392768b0980811b19b0095b093
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-14 16:19:10 +0000

    feat: consistent spectrogram plotting with PanelStacker

---
+++ spectre/spectrogram/PanelStacker.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
+from spectre.spectrogram.Panels import Panels
+ 
+
+class PanelStacker(Panels):  
+    def __init__(self, S):
+        super().__init__(S)
+    
+    def create_figure(self, fig: Figure, panel_types: str):
+
+        if len(panel_types)==1:
+            is_one_panel=True
+        else:
+            is_one_panel=False
+        
+        # # Create a figure with subplots for plots and colorbars
+        # two columns, one for the panels, and an optional column for the colorbars
+        axs = fig.subplots(len(panel_types), 2, 
+                            gridspec_kw={'width_ratios': [3, 0.1], 
+                            'wspace': 0.05},
+                            squeeze=False
+                            )
+        # Iterate over the plot types and their respective axes
+        for panel_index, panel_type in enumerate(panel_types):
+            
+            ax = axs[panel_index, 0]  # Plot on the first column
+            cax = axs[panel_index, 1]  # Colorbar on the second column
+
+            # by default, turn the color axis off
+            cax.axis('off')  # Initially turn off the colorbar axis; it will be turned on if needed
+
+            # Get the plotting function
+            plot_method = self.get_plot_method(panel_type)
+
+            # Call the plotting function with its specific kwargs
+            plot_method(ax=ax, cax=cax)  # Pass both plot and colorbar axes
+
+            # Hide x-axis labels for all but the bottom plot
+            if panel_index < len(panel_types) - 1:
+                ax.tick_params(labelbottom=False)
+            
+            if panel_index ==len(panel_types)-1 or is_one_panel:
+                ax.set_xlabel('Time [GMT]', size=self.fsize_head)
+
+
+        
---
+++ spectre/spectrogram/Panels.py
@@ -0,0 +1,121 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.colors import LogNorm
+from matplotlib.axes import Axes
+from math import floor
+
+
+class Panels:
+    def __init__(self, S):
+        self.S = S
+
+        self.panel_type_dict = {
+                "integrated_power": self.integrated_power,
+                "dBb": self.dBb,
+                "raw": self.raw,
+                "rawlog": self.rawlog,
+            }
+        
+        self.valid_panel_types = self.panel_type_dict.keys()
+        
+        self.fsize_head=20
+        self.fsize=15
+        self.cmap = 'gnuplot2'
+        self.seconds_interval = floor(self.S.time_seconds[-1]/3)
+
+       
+    def get_plot_method(self, panel_type: str):
+        plot_method = self.panel_type_dict.get(panel_type, None)
+        if plot_method is None:
+            raise KeyError(f"{panel_type} is not valid. Expected one of {self.valid_panel_types}.")
+        return plot_method
+
+
+    def integrated_power(self, ax: Axes, cax: Axes) -> None:
+        datetimes = self.S.datetimes
+        power = self.S.integrated_power()
+
+        ax.step(datetimes, power, where='mid')
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+        ax.xaxis.set_major_locator(mdates.SecondLocator(interval=self.seconds_interval))
+        ax.tick_params(axis='x', labelsize=self.fsize)
+        ax.tick_params(axis='y', labelsize=self.fsize)
+        ax.set_ylabel('Normalised Power', size=self.fsize_head)
+
+
+    def dBb(self, ax: Axes, cax: Axes) -> None:
+        datetimes = self.S.datetimes
+        freq_MHz = self.S.freq_MHz
+
+        if self.S.bvect is None:
+            raise ValueError(f"Cannot plot in units of dBb, bvect is not specified. Got bvect={self.S.bvect}")
+
+        mags = self.S.mags_as_dBb()
+
+        vmin = -1
+        vmax = 5
+
+        pcolor_plot = ax.pcolormesh(datetimes, 
+                                    freq_MHz, 
+                                    mags, 
+                                    vmin=vmin, 
+                                    vmax=vmax, 
+                                    cmap=self.cmap)
+        
+        # Format the x-axis to display time in HH:MM:SS
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+        ax.xaxis.set_major_locator(mdates.SecondLocator(interval=self.seconds_interval))
+        # Assign the x and y labels with specified font size
+        ax.set_ylabel('Frequency [MHz]', size=self.fsize_head)
+        # Format the x and y tick labels with specified font size
+        ax.tick_params(axis='x', labelsize=self.fsize)
+        ax.tick_params(axis='y', labelsize=self.fsize)
+
+        cax.axis("On")
+        cbar = plt.colorbar(pcolor_plot,ax=ax,cax=cax)
+        cbar.set_label('dB above background', size=self.fsize_head)
+        cbar.set_ticks(range(vmin, vmax+1, 1))
+
+    
+    def rawlog(self, ax: Axes, cax: Axes) -> None:
+        freq_MHz = self.S.freq_MHz
+        datetimes = self.S.datetimes
+        mags = self.S.mags
+
+        # Plot the spectrogram with LogNorm
+        pcolor_plot = ax.pcolormesh(datetimes, 
+                                    freq_MHz, 
+                                    mags, 
+                                    norm=LogNorm(vmin=np.min(mags[mags > 0]), vmax=np.max(mags)),
+                                    cmap=self.cmap)
+
+        # Format the x-axis to display time in HH:MM:SS
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+        ax.xaxis.set_major_locator(mdates.SecondLocator(interval=self.seconds_interval))
+        # Assign the x and y labels with specified font size
+        ax.set_ylabel('Frequency [MHz]', size=self.fsize_head)
+        # Format the x and y tick labels with specified font size
+        ax.tick_params(axis='x', labelsize=self.fsize)
+        ax.tick_params(axis='y', labelsize=self.fsize)
+        cax.axis("On")
+        cbar = plt.colorbar(pcolor_plot,ax=ax,cax=cax)
+
+
+    def raw(self, ax: Axes ,cax: Axes) -> None:
+        freq_MHz = self.S.freq_MHz
+        datetimes = self.S.datetimes
+        mags = self.S.mags
+
+        pcolor_plot = ax.pcolormesh(datetimes, 
+                                    freq_MHz, 
+                                    mags, 
+                                    cmap=self.cmap)
+        # Format the x-axis to display time in HH:MM:SS
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+        ax.xaxis.set_major_locator(mdates.SecondLocator(interval=self.seconds_interval))
+        # Assign the x and y labels with specified font size
+        ax.set_ylabel('Frequency [MHz]', size=self.fsize_head)
+        # Format the x and y tick labels with specified font size
+        ax.tick_params(axis='x', labelsize=self.fsize)
+        ax.tick_params(axis='y', labelsize=self.fsize)
