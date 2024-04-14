5baee5911639dfa2499e8972daa7700a6649a390
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-14 12:16:02 +0000

    fix: corrected np.array to np.ndarray

--- spectre/chunks/library/default/ChunkBin.py
+++ spectre/chunks/library/default/ChunkBin.py
@@ -7,7 +7,7 @@ class ChunkBin(ChunkExt):
     def __init__(self, chunk_start_time: str, tag: str, chunks_dir: str):
         super().__init__(chunk_start_time, tag, ".bin", chunks_dir)
 
-    def get_IQ_data(self) -> np.array:
+    def get_IQ_data(self) -> np.ndarray:
         try:
             with open(self.get_path(), "rb") as fh:
                 IQ_data = np.fromfile(fh, dtype=np.complex64)
