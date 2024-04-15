b99c7df7d6e38715dbc383dfa3585b962047f9b1
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-15 19:13:22 +0000

    fix: fixed paths

--- host/cfg/CONFIG.py
+++ host/cfg/CONFIG.py
@@ -5,4 +5,4 @@ chunks_dir = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'chunks')
 path_to_capture_log = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'cfg', 'capture.log')
 
 path_to_start_capture = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'capture_scripts', 'start_capture.py')
-path_to_post_proc = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'capture_scripts', 'post_proc.py')
+path_to_start_watcher = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'capture_scripts', 'start_watcher.py')
