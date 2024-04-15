fix: handling of units

spectre/spectrogram/Spectrogram.py
@@ -39,7 +39,9 @@ class Spectrogram:
 
         self.chunk_start_time = chunk_start_time
         self.tag = tag
-        self.bvect = kwargs.get("bvect", None)
+
+        default_bvect = self.total_time_average()
+        self.bvect = kwargs.get("bvect", default_bvect)
         self.units = kwargs.get("units", None)
 
         self.chunk_start_datetime = datetime.strptime(self.chunk_start_time, CONFIG.default_time_format)
@@ -109,8 +111,16 @@ class Spectrogram:
 
     def dynamic_spectra_as_dBb(self):
         bvect_array = np.outer(self.bvect, np.ones(self.shape[1]))
-        dynamic_spectra_as_dBb = 10 * np.log10(self.dynamic_spectra / bvect_array)
+
+        if self.units == "amplitude":
+            dynamic_spectra_as_dBb = 10 * np.log10(self.dynamic_spectra / bvect_array)
+        elif self.units == "power":
+            dynamic_spectra_as_dBb = 20 * np.log10(self.dynamic_spectra / bvect_array)
+        else:
+            raise ValueError(f"Units not specified, uncertain decibel conversions!")
+        
         return dynamic_spectra_as_dBb
+    
 
     def stack_panels(self, fig: Figure, panel_types: list[str]) -> None:
         PanelStacker(self).create_figure(fig, panel_types)

spectre/utils/fits_helpers.py
@@ -7,16 +7,16 @@ from spectre.utils import datetime_helpers
 
 
 # Function to create a FITS file with the specified structure
-def save_spectrogram(spectrogram, fits_config: dict, path_to_fits: str):
+def save_spectrogram(S, fits_config: dict, path_to_fits: str):
     # Primary HDU with data
-    primary_data = spectrogram.dynamic_spectra.astype(dtype=np.float64) 
+    primary_data = S.dynamic_spectra.astype(dtype=np.float64) 
     primary_hdu = fits.PrimaryHDU(primary_data)
 
     primary_hdu.header.set('SIMPLE', True, 'file does conform to FITS standard')
     primary_hdu.header.set('BITPIX', -64, 'number of bits per data pixel')
     primary_hdu.header.set('NAXIS', 2, 'number of data axes')
-    primary_hdu.header.set('NAXIS1', spectrogram.dynamic_spectra.shape[1], 'length of data axis 1')
-    primary_hdu.header.set('NAXIS2', spectrogram.dynamic_spectra.shape[0], 'length of data axis 2')
+    primary_hdu.header.set('NAXIS1', S.dynamic_spectra.shape[1], 'length of data axis 1')
+    primary_hdu.header.set('NAXIS2', S.dynamic_spectra.shape[0], 'length of data axis 2')
     primary_hdu.header.set('EXTEND', True, 'FITS dataset may contain extensions')
 
     # Add comments
@@ -31,18 +31,18 @@ def save_spectrogram(spectrogram, fits_config: dict, path_to_fits: str):
     for comment in comments:
         primary_hdu.header.add_comment(comment)
 
-    start_datetime = spectrogram.datetimes[0]
+    start_datetime = S.datetimes[0]
     start_date = start_datetime.strftime("%Y-%m-%d")
     start_time = start_datetime.strftime("%H:%M:%S.%f")
 
-    end_datetime = spectrogram.datetimes[-1]
+    end_datetime = S.datetimes[-1]
     end_date = end_datetime.strftime("%Y-%m-%d")
     end_time = end_datetime.strftime("%H:%M:%S.%f")
 
     primary_hdu.header.set('DATE', start_date, 'time of observation')
     primary_hdu.header.set('CONTENT', f'{start_date} power spectral density, grso (GLASGOW)', 'title of image')
     primary_hdu.header.set('ORIGIN', f'{fits_config.get("ORIGIN")}')
-    primary_hdu.header.set('TELESCOP', f'{fits_config.get("TELESCOP")} tag: {spectrogram.tag}', 'type of instrument')
+    primary_hdu.header.set('TELESCOP', f'{fits_config.get("TELESCOP")} tag: {S.tag}', 'type of instrument')
     primary_hdu.header.set('INSTRUME', f'{fits_config.get("INSTRUME")}') 
     primary_hdu.header.set('OBJECT', f'{fits_config.get("OBJECT")}', 'object description')
 
@@ -53,20 +53,20 @@ def save_spectrogram(spectrogram, fits_config: dict, path_to_fits: str):
 
     primary_hdu.header.set('BZERO', 0, 'scaling offset')
     primary_hdu.header.set('BSCALE', 1, 'scaling factor')
-    primary_hdu.header.set('BUNIT', " ... ", 'z-axis title') # This should be specified with the actual unit
+    primary_hdu.header.set('BUNIT', f"{S.units}", 'z-axis title') 
 
-    primary_hdu.header.set('DATAMIN', np.nanmin(spectrogram.dynamic_spectra), 'minimum element in image')
-    primary_hdu.header.set('DATAMAX', np.nanmax(spectrogram.dynamic_spectra), 'maximum element in image')
+    primary_hdu.header.set('DATAMIN', np.nanmin(S.dynamic_spectra), 'minimum element in image')
+    primary_hdu.header.set('DATAMAX', np.nanmax(S.dynamic_spectra), 'maximum element in image')
 
     primary_hdu.header.set('CRVAL1', f'{datetime_helpers.seconds_of_day(start_datetime)}', 'value on axis 1 at reference pixel [sec of day]')
     primary_hdu.header.set('CRPIX1', 0, 'reference pixel of axis 1')
     primary_hdu.header.set('CTYPE1', 'TIME [UT]', 'title of axis 1')
-    primary_hdu.header.set('CDELT1', spectrogram.time_res_seconds, 'step between first and second element in x-axis')
+    primary_hdu.header.set('CDELT1', S.time_res_seconds, 'step between first and second element in x-axis')
 
     primary_hdu.header.set('CRVAL2', 0, 'value on axis 2 at reference pixel')
     primary_hdu.header.set('CRPIX2', 0, 'reference pixel of axis 2')
     primary_hdu.header.set('CTYPE2', 'Frequency [MHz]', 'title of axis 2')
-    primary_hdu.header.set('CDELT2', spectrogram.freq_res_MHz, 'step between first and second element in axis')
+    primary_hdu.header.set('CDELT2', S.freq_res_MHz, 'step between first and second element in axis')
 
     primary_hdu.header.set('OBS_LAT', f'{fits_config.get("OBS_LAT")}', 'observatory latitude in degree')
     primary_hdu.header.set('OBS_LAC', 'N', 'observatory latitude code {N,S}')
@@ -76,8 +76,8 @@ def save_spectrogram(spectrogram, fits_config: dict, path_to_fits: str):
 
 
     # Wrap arrays in an additional dimension to mimic the e-CALLISTO storage
-    time_array_wrapped = np.array([spectrogram.time_seconds.astype(np.float64)])
-    freqs_MHz_wrapped = np.array([spectrogram.freq_MHz.astype(np.float64)])
+    time_array_wrapped = np.array([S.time_seconds.astype(np.float64)])
+    freqs_MHz_wrapped = np.array([S.freq_MHz.astype(np.float64)])
     
     # Binary Table HDU (extension)
     col1 = fits.Column(name='TIME', format='PD()', array=time_array_wrapped)