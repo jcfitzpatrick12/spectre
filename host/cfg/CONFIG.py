f9bfcdb60e211c867cefe20111b21bfb2ad4796f
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-16 15:37:08 +0000

    chore: update paths

--- host/cfg/CONFIG.py
+++ host/cfg/CONFIG.py
@@ -4,5 +4,6 @@ json_configs_dir = os.path.join(os.environ['SPECTREPARENTPATH'],"host", "cfg", "
 chunks_dir = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'chunks')
 path_to_capture_log = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'cfg', 'capture.log')
 
-path_to_start_capture = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'capture_scripts', 'start_capture.py')
-path_to_start_watcher = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'capture_scripts', 'start_watcher.py')
+path_to_capture_scripts = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'capture_scripts')
+path_to_start_capture = os.path.join(path_to_capture_scripts, 'start_capture.py')
+path_to_start_watcher = os.path.join(path_to_capture_scripts, 'start_watcher.py')
