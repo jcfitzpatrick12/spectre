c7847a49a7832a1c9f6131b87779cae4167ac529
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-25 20:51:33 +0100

    fix: correct starter daily capture tag

--- host/scripts/daily_capture.sh
+++ host/scripts/daily_capture.sh
@@ -5,7 +5,7 @@ export PYTHONPATH="${SPECTREPARENTPATH}:${PYTHONPATH}"
 # ensure that spectre is recognised as a command in the cron environment
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-spectre capture start-watcher -t kerbin
-spectre capture start --receiver RSPDuo --mode tuner_1_fixed --tag kerbin
+spectre capture start-watcher -t jool
+spectre capture start --receiver RSPDuo --mode tuner_1_fixed --tag jool
 sleep 36000s
 spectre capture stop
