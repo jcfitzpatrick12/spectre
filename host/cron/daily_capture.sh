e514e2cccf0eb7a6ba3a26467098c09ec1a015b9
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-26 21:34:57 +0100

    fix: cronjob fix due to display env not being set

--- host/cron/daily_capture.sh
+++ host/cron/daily_capture.sh
@@ -5,6 +5,8 @@ export SPECTREPARENTPATH=/home/spectre
 export PYTHONPATH="${SPECTREPARENTPATH}:${PYTHONPATH}"
 # ensure that spectre is recognised as a command in the cron environment
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+# set the display environment variable
+export DISPLAY=:0
 
 spectre capture start-watcher -t jool
 spectre capture start --receiver RSPDuo --mode tuner_1_fixed --tag jool
