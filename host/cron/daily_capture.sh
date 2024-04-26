e7456c703d5bd9169e72e0ed3d87feaca605e32f
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-26 20:30:19 +0100

    fix: ensure chunks dir is created prior to daily capture calls

--- host/cron/daily_capture.sh
+++ host/cron/daily_capture.sh
@@ -5,6 +5,9 @@ export PYTHONPATH="${SPECTREPARENTPATH}:${PYTHONPATH}"
 # ensure that spectre is recognised as a command in the cron environment
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+# ensure the chunks directory is created prior to calling capture
+mkdir -p $SPECTREPARENTPATH/host/chunks
+
 spectre capture start-watcher -t jool
 spectre capture start --receiver RSPDuo --mode tuner_1_fixed --tag jool
 sleep 36000s
