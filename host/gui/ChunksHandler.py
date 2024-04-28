ba6de0055a46b4b22d8c6724e9e832390dcd2654
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-28 17:24:01 +0000

    feat: modularised GUI

---
+++ host/gui/ChunksHandler.py
@@ -0,0 +1,59 @@
+from host.cfg import CONFIG
+from spectre.chunks.Chunks import Chunks
+from spectre.spectrogram import factory
+from spectre.spectrogram.Panels import Panels
+
+class ChunksHandler:
+    def __init__(self, tag):
+        self.tag = tag
+        self.chunks = Chunks(tag, CONFIG.chunks_dir, CONFIG.json_configs_dir)
+        self.set_default_spectrogram()
+
+    def update_tag(self, tag):
+        self.tag = tag
+        self.chunks = Chunks(tag, CONFIG.chunks_dir, CONFIG.json_configs_dir)
+        self.set_default_spectrogram()
+
+    def get_spectrogram(self, entry_values):
+        start_time = entry_values['start_time'].get()
+        end_time = entry_values['end_time'].get()
+        lower_freq = float(entry_values['lower_freq'].get())
+        upper_freq = float(entry_values['upper_freq'].get())
+        avg_over_int_time = int(entry_values['avg_over_int_time'].get())
+        avg_over_int_freq = int(entry_values['avg_over_int_freq'].get())
+        S = self.chunks.build_spectrogram_from_range(start_time, end_time)
+        S = factory.frequency_chop(S, lower_freq, upper_freq)
+        S = factory.frequency_average(S, avg_over_int_freq)
+        S = factory.time_average(S, avg_over_int_time)
+        return S
+
+    def set_default_spectrogram(self):
+        chunks_list = list(self.chunks.dict.values())
+        for chunk in reversed(chunks_list):
+            if chunk.fits.exists():
+                self.default_S = chunk.fits.load_spectrogram()
+                return
+        raise ValueError(f"No fits files exist for tag {self.tag}")
+
+    def get_field_defaults(self):
+        return {
+            'start_time': self.default_S.datetimes[0].strftime(CONFIG.default_time_format),
+            'end_time': self.default_S.datetimes[-1].strftime(CONFIG.default_time_format),
+            'lower_freq': round(self.default_S.freq_MHz[0], 2),
+            'upper_freq': round(self.default_S.freq_MHz[-1], 2),
+            'avg_over_int_time': 1,
+            'avg_over_int_freq': 1
+        }
+
+    def get_field_labels(self):
+        return {
+            'start_time': "Start time: ",
+            'end_time': "End time: ",
+            'lower_freq': "Lower frequency [MHz]: ",
+            'upper_freq': "Higher frequency [MHz]: ",
+            'avg_over_int_time': "Average over (time): ",
+            'avg_over_int_freq': "Average over (frequency): ",
+        }
+
+    def get_panel_types(self):
+        return Panels(self.default_S).panel_type_dict.keys()
--- host/gui/SPECTRECompanion.py
+++ host/gui/SPECTRECompanion.py
@@ -1,129 +1,50 @@
 import tkinter as tk
-from tkinter import ttk
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
-
-from spectre.chunks.Chunks import Chunks
-from spectre.spectrogram import factory
-from host.cfg import CONFIG
-from spectre.spectrogram.Panels import Panels
+from host.gui.ui_components import create_labeled_entry, setup_plot, create_tickbox_frame
+from host.gui.ChunksHandler import ChunksHandler
 
 class SPECTRECompanion:
     def __init__(self, master, initial_tag: str):
         self.master = master
         self.master.title("SPECTRE Companion")
         self.tag = initial_tag
-        self.initialize_components()
-
-    def initialize_components(self):
-        self.load_chunks()
+        self.chunks_handler = ChunksHandler(self.tag)
+        self.entries = {}
         self.setup_widgets()
-        self.set_default_values()
-        self.update_entry_fields()
-        self.populate_panel_types()
-
-
-    def load_chunks(self):
-        self.chunks = Chunks(self.tag, CONFIG.chunks_dir, CONFIG.json_configs_dir)
 
     def setup_widgets(self):
-        self.tag_entry = self.create_labeled_entry("Tag: ", self.tag, 0)
+        self.update_widgets()
+        self.update_entries(self.chunks_handler.get_field_defaults())
+
+
+    def update_widgets(self):
         tk.Button(self.master, text="Update", command=self.update_tag_from_entry).grid(row=0, column=2, sticky="ew")
         tk.Button(self.master, text="Plot", command=self.plot_data).grid(row=8, column=0, sticky="ew")
-
-        self.figure = Figure(figsize=(15, 10))
-        self.canvas = FigureCanvasTkAgg(self.figure, self.master)
-        self.canvas.get_tk_widget().grid(row=9, column=0, columnspan=3)
-
-
-    def create_labeled_entry(self, label, default, row):
-        tk.Label(self.master, text=label).grid(row=row, column=0)
-        entry = tk.Entry(self.master)
-        entry.grid(row=row, column=1)
-        entry.insert(0, default)
-        return entry
+        self.figure, self.canvas = setup_plot(self.master)
+        self.panel_type_frame, self.panel_type_vars = create_tickbox_frame(self.master, self.chunks_handler.get_panel_types(), "Plot Types")
 
 
     def update_tag_from_entry(self):
-        self.tag = self.tag_entry.get()
-        self.load_chunks()
-        self.set_default_values()
-        self.update_entry_fields()
-        self.populate_panel_types()
+        self.tag = self.entries['tag'].get()
+        self.chunks_handler.update_tag(self.tag)
+        self.update_entries(self.chunks_handler.get_field_defaults())
 
 
-    def set_default_values(self):
-        chunks_list = list(self.chunks.dict.values())
-        for chunk in reversed(chunks_list):
-            if chunk.fits.exists():
-                default_S = chunk.fits.load_spectrogram()
-                self.default_values = {
-                    'start_time': default_S.datetimes[0].strftime(CONFIG.default_time_format),
-                    'end_time': default_S.datetimes[-1].strftime(CONFIG.default_time_format),
-                    'lower_freq': round(default_S.freq_MHz[0], 2),
-                    'upper_freq': round(default_S.freq_MHz[-1], 2),
-                    'avg_over_int_time': 1,
-                    'avg_over_int_freq': 1
-                }
-                self.panel_types = Panels(default_S).panel_type_dict.keys()
-                return
-        raise ValueError(f"No fits files exist for tag {self.tag}")
-
-
-    def update_entry_fields(self):
-        self.entries = {}
-        fields = ['start_time', 'end_time', 'lower_freq', 'upper_freq', 'avg_over_int_time', 'avg_over_int_freq']
-        field_titles_dict = self.get_field_titles_dict()
-        for i, field in enumerate(fields, start=1):
-            self.entries[field] = self.create_labeled_entry(field_titles_dict[field], self.default_values[field], i)
-
-
-    def get_field_titles_dict(self):
-        
-        field_titles_dict = {
-            'start_time': "Start time: ",
-            'end_time': "End time: ",
-            'lower_freq': "Lower frequency [MHz]: ",
-            'upper_freq': "Higher frequency [MHz]: ",
-            'avg_over_int_time': "Average over int (time): ",
-            'avg_over_int_freq': "Average over int (frequency): ",
-        }
-
-        return field_titles_dict
-    
-
-    def populate_panel_types(self):
-        if not hasattr(self, 'panel_type_frame'):
-            self.panel_type_frame = tk.LabelFrame(self.master, text="Plot Types:")
-            self.panel_type_frame.grid(row=7, column=0, columnspan=3)
-
-        for widget in self.panel_type_frame.winfo_children():
-            widget.destroy()
-
-        self.panel_type_vars = {ptype: tk.BooleanVar() for ptype in self.panel_types}
-        for ptype in self.panel_types:
-            tk.Checkbutton(self.panel_type_frame, text=ptype, variable=self.panel_type_vars[ptype]).pack(side=tk.LEFT)
-
-
-    def get_selected_panel_types(self):
-        return [ptype for ptype, var in self.panel_type_vars.items() if var.get()]
-
+    def update_entries(self, field_values):
+        field_labels = self.chunks_handler.get_field_labels()
+        for i, (field, value) in enumerate(field_values.items(), start=1):
+            if field in self.entries:
+                entry = self.entries[field]
+                entry.delete(0, tk.END)
+                entry.insert(0, value)
+            else:
+                self.entries[field] = create_labeled_entry(self.master, field_labels[field], value, i)
 
     def plot_data(self):
-        S = self.get_spectrogram()
-        self.figure.clear()
-        S.stack_panels(self.figure, self.get_selected_panel_types())
+        selected_panels = [ptype for ptype, var in self.panel_type_vars.items() if var.get()]
+        spectrogram = self.chunks_handler.get_spectrogram(self.entries)
+        self.figure.clear()  # Clear the existing plot
+        spectrogram.stack_panels(self.figure, selected_panels)
         self.canvas.draw()
-
-
-    def get_spectrogram(self):
-        start_time, end_time = self.entries['start_time'].get(), self.entries['end_time'].get()
-        lower_freq, upper_freq = float(self.entries['lower_freq'].get()), float(self.entries['upper_freq'].get())
-        S = self.chunks.build_spectrogram_from_range(start_time, end_time)
-        S = factory.frequency_chop(S, lower_freq, upper_freq)
-        S = factory.frequency_average(S, int(self.entries['avg_over_int_freq'].get()))
-        S = factory.time_average(S, int(self.entries['avg_over_int_time'].get()))
-        return S
 
 
 def main(tag):
