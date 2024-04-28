eb95b2880b35d978cc5915d61ca7a5bb7e29e322
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-28 17:24:15 +0000

    feat: modularised GUI

--- host/gui/ui_components.py
+++ host/gui/ui_components.py
@@ -9,22 +9,18 @@ def create_labeled_entry(master, label, default, row):
     entry.insert(0, default)
     return entry
 
-
 def setup_plot(master):
     figure = Figure(figsize=(17, 10))
     canvas = FigureCanvasTkAgg(figure, master)
     canvas.get_tk_widget().grid(row=9, column=0, columnspan=4)
     return figure, canvas
 
-
 def create_tickbox_frame(master, options, title="Options", row=7, column=0, columnspan=3):
     frame = tk.LabelFrame(master, text=title)
     frame.grid(row=row, column=column, columnspan=columnspan)
-
     var_dict = {}
     for option in options:
         var = tk.BooleanVar()
         tk.Checkbutton(frame, text=option, variable=var).pack(side=tk.LEFT)
         var_dict[option] = var
-
     return frame, var_dict
