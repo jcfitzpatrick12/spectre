b15846dc2ebad5bcb3dda8a8414282808973fb94
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-04-28 10:22:38 +0000

    feat: add basic key-value check to test receiver

--- spectre/receivers/library/Test/mounts/Capture.py
+++ spectre/receivers/library/Test/mounts/Capture.py
@@ -2,6 +2,7 @@ from spectre.receivers.CaptureMount import CaptureMount
 from spectre.receivers.mount_register import register_capture_mount
 
 from spectre.receivers.library.Test.gr import cosine_signal
+from spectre.receivers.library.Test.gr import dummy
 
 @register_capture_mount("Test")
 class Capture(CaptureMount):
@@ -12,6 +13,8 @@ class Capture(CaptureMount):
     def set_capture_methods(self) -> None:
         self.capture_methods = {
             "cosine_signal": self.cosine_signal,
+            "key_value_test": self.dummy,
+
         }
 
     def cosine_signal(self, capture_configs: list) -> None:
@@ -22,5 +25,13 @@ class Capture(CaptureMount):
         capture_config = capture_configs[0]
         cosine_signal.main(capture_config)
         return
+    
 
-
+    def dummy(self, capture_configs: list) -> None:
+        num_capture_configs = len(capture_configs)
+        if num_capture_configs > 1:
+            raise ValueError(f"Expected 1 capture config. Received {num_capture_configs}")
+        # take the first (and now verified only) capture config in the list
+        capture_config = capture_configs[0]
+        dummy.main(capture_config)
+        return
--- spectre/receivers/library/Test/mounts/CaptureConfig.py
+++ spectre/receivers/library/Test/mounts/CaptureConfig.py
@@ -1,6 +1,7 @@
 from spectre.receivers.CaptureConfigMount import CaptureConfigMount
 from spectre.receivers.mount_register import register_capture_config_mount
 from spectre.utils import validator_helpers
+from spectre.utils import dict_helpers
 
 
 @register_capture_config_mount("Test")
@@ -25,18 +26,42 @@ class CaptureConfig(CaptureConfigMount):
                 'event_handler_key': str, # tag will map to event handler with this key during post processing
                 'watch_extension': str # postprocessing will call proc defined in event handler for files appearing with this extension
             },
+            
+            "key_value_test": {
+                'int_key': int,
+                'str_key': str,
+                'dict_key': dict,
+                'float_key': float,
+                'bool_key': bool, 
+            }
         }
 
 
     def set_validators(self) -> None:
         self.validators = {
             "cosine_signal": self.cosine_validator,
+            "key_value_test": self.key_value_test_validator,
         }
 
     
     def cosine_validator(self, capture_config_dict: dict) -> None:
         return
 
+    
+    def key_value_test_validator(self, capture_config_dict: dict) -> None:
+        print("Performing key value test.")
+        template = self.templates.get('key_value_test')
+        if template is None:
+            raise ValueError("Could not find template for the key value test.")
+        
+        dict_helpers.validate_keys(capture_config_dict, template)
+        print("Keys verified.")
+
+        dict_helpers.validate_types(capture_config_dict, template)
+        print("Values verified.")
+
+        print("Validated capture config is consistent with the template")
+        return
 
 
     
