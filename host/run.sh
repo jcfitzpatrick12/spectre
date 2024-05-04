00e0c20e36b736e34a536eb2af605b511c5b4801
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-05-04 22:11:09 +0100

    feat: removed named mounts, and replace with bind mounts

--- host/run.sh
+++ host/run.sh
@@ -6,14 +6,10 @@ if [ ! -d "$SPECTREPARENTPATH/chunks" ]; then
     mkdir -p "$SPECTREPARENTPATH/chunks"
 fi
 
-# Create named volumes for configuration and logs
-docker volume create spectre-host-config-vol
-docker volume create spectre-host-logs-vol
-
 # Run the Docker container with GUI support
 docker run --name spectre-host-container -it --rm \
-    -v spectre-host-config-vol:/home/spectre/cfg \
-    -v spectre-host-logs-vol:/home/spectre/logs \
+    -v $SPECTREPARENTPATH/cfg:/home/spectre/cfg \
+    -v $SPECTREPARENTPATH/logs:/home/spectre/logs \
     -v $SPECTREPARENTPATH/chunks:/home/spectre/chunks \
     -v /dev/shm:/dev/shm \
     spectre-host
