f19580c169007698440d313cd61abc398dd5b32c
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-05-04 20:57:30 +0100

    feat: precautionary directory creation and env variable setting

--- host/run.sh
+++ host/run.sh
@@ -1,3 +1,11 @@
+# set SPECTREPARENTPATH to the parent directory of this script
+export SPECTREPARENTPATH=$(dirname "$(dirname "$(realpath "$0")")")
+
+# Check if the directory $SPECTREPARENTPATH/chunks exists; if not, create it
+if [ ! -d "$SPECTREPARENTPATH/chunks" ]; then
+    mkdir "$SPECTREPARENTPATH/chunks"
+fi
+
 # Create a volume so that storage persists
 docker volume create spectre-host-vol
 
@@ -5,5 +13,6 @@ docker volume create spectre-host-vol
 docker run --name spectre-host-container -it --rm \
     --mount type=volume,src=spectre-host-vol,target=/home \
     -v /dev/shm:/dev/shm \
+    -v $SPECTREPARENTPATH/chunks:$SPECTREPARENTPATH \
     spectre-host
 
