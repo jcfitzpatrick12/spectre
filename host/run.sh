78fa441383399d0d6f43510b02dd23c07020e1ef
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-05-04 15:40:29 +0000

    feat: remove gui capabilities from spectre-host during build

--- host/run.sh
+++ host/run.sh
@@ -1,6 +1,3 @@
-# Enable xhost for the local machine only, safer than 'xhost +'
-xhost local:
-
 # Create a volume so that storage persists
 docker volume create spectre-host-vol
 
@@ -8,9 +5,5 @@ docker volume create spectre-host-vol
 docker run --name spectre-host-container -it --rm \
     --mount type=volume,src=spectre-host-vol,target=/home \
     -v /dev/shm:/dev/shm \
-    -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
     spectre-host
 
-# Reset xhost
-xhost -
