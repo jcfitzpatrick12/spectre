0b70edf675e1eab0a33ba1b2c4a09efb7a3f1604
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-05-04 21:46:01 +0100

    fix: seperate named volume and bind mounts

--- host/run.sh
+++ host/run.sh
@@ -1,18 +1,19 @@
-# set SPECTREPARENTPATH to the parent directory of this script
+# Set SPECTREPARENTPATH to the parent directory of this script
 export SPECTREPARENTPATH=$(dirname "$(dirname "$(realpath "$0")")")
 
 # Check if the directory $SPECTREPARENTPATH/chunks exists; if not, create it
 if [ ! -d "$SPECTREPARENTPATH/chunks" ]; then
-    mkdir "$SPECTREPARENTPATH/chunks"
+    mkdir -p "$SPECTREPARENTPATH/chunks"
 fi
 
-# Create a volume so that storage persists
-docker volume create spectre-host-vol
+# Create named volumes for configuration and logs
+docker volume create spectre-host-config-vol
+docker volume create spectre-host-logs-vol
 
 # Run the Docker container with GUI support
 docker run --name spectre-host-container -it --rm \
-    --mount type=volume,src=spectre-host-vol,target=/home \
+    -v spectre-host-config-vol:/home/spectre/cfg \
+    -v spectre-host-logs-vol:/home/spectre/logs \
+    -v $SPECTREPARENTPATH/chunks:/home/spectre/chunks \
     -v /dev/shm:/dev/shm \
-    -v $SPECTREPARENTPATH/chunks:$SPECTREPARENTPATH \
     spectre-host
-
