ce34af2ea05de3c8daf9bc33cf66ddd3daee42dc
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-05-04 20:28:21 +0100

    feat: improve error handling for SPECTREPARENTPATH env variable

--- cfg/CONFIG.py
+++ cfg/CONFIG.py
@@ -1,11 +1,17 @@
 import os
 
-default_time_format = "%Y-%m-%dT%H:%M:%S"
-chunks_dir = os.path.join(os.environ['SPECTREPARENTPATH'], 'chunks')
-path_to_capture_log = os.path.join(os.environ['SPECTREPARENTPATH'], 'logs', 'capture.log')
-json_configs_dir = os.path.join(os.environ['SPECTREPARENTPATH'], "cfg", "json_configs")
 
-path_to_capture_scripts = os.path.join(os.environ['SPECTREPARENTPATH'], 'host', 'capture_scripts')
+SPECTREPARENTPATH = os.environ.get("SPECTREPARENTPATH")
+if SPECTREPARENTPATH is None:
+    raise ValueError("The environment variable SPECTREPARENTPATH has not been set.")
+
+
+default_time_format = "%Y-%m-%dT%H:%M:%S"
+chunks_dir = os.path.join(SPECTREPARENTPATH, 'chunks')
+path_to_capture_log = os.path.join(SPECTREPARENTPATH, 'logs', 'capture.log')
+json_configs_dir = os.path.join(SPECTREPARENTPATH, "cfg", "json_configs")
+
+path_to_capture_scripts = os.path.join(SPECTREPARENTPATH, 'host', 'capture_scripts')
 path_to_start_capture = os.path.join(path_to_capture_scripts, 'start_capture.py')
 path_to_start_watcher = os.path.join(path_to_capture_scripts, 'start_watcher.py')
 
