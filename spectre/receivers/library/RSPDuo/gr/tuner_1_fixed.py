cc081e9c1c4d7b59bd7b2069c68929d29ff36763
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-05-04 14:38:58 +0000

    feat: remove gui dependency for RSPDuo tuner_1_fixed gr script

--- spectre/receivers/library/RSPDuo/gr/tuner_1_fixed.py
+++ spectre/receivers/library/RSPDuo/gr/tuner_1_fixed.py
@@ -5,27 +5,14 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Not titled yet
+# Title: Options 0
 # GNU Radio version: 3.10.1.1
-
-from packaging.version import Version as StrictVersion
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print("Warning: failed to XInitThreads()")
 
 from gnuradio import gr
 from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
-from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
@@ -34,40 +21,11 @@ from gnuradio import spectre
 
 
 
-from gnuradio import qtgui
 
-class tuner_1_fixed(gr.top_block, Qt.QWidget):
+class tuner_1_fixed(gr.top_block):
 
-    def __init__(self, capture_config):
-        gr.top_block.__init__(self, "Capture in progress.", catch_exceptions=True)
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Capture in progress")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "default")
-
-        try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except:
-            pass
+    def __init__(self, capture_config: dict):
+        gr.top_block.__init__(self, "tuner_1_fixed", catch_exceptions=True)
 
         ##################################################
         # Unpack capture config
@@ -80,11 +38,6 @@ class tuner_1_fixed(gr.top_block, Qt.QWidget):
         bandwidth = capture_config['bandwidth']
         IF_gain = capture_config['IF_gain']
         RF_gain = capture_config['RF_gain']
-
-        ##################################################
-        # Variables
-        ##################################################
-        self.samp_rate = samp_rate
 
         ##################################################
         # Blocks
@@ -125,51 +78,23 @@ class tuner_1_fixed(gr.top_block, Qt.QWidget):
         self.connect((self.sdrplay3_rspduo_0, 0), (self.spectre_batched_file_sink_0, 0))
 
 
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "default")
-        self.settings.setValue("geometry", self.saveGeometry())
-        self.stop()
-        self.wait()
-
-        event.accept()
-
-    def get_samp_rate(self):
-        return self.samp_rate
-
-    def set_samp_rate(self, samp_rate):
-        self.samp_rate = samp_rate
-        self.sdrplay3_rspduo_0.set_sample_rate(self.samp_rate)
 
 
 
-
-def main(capture_config, top_block_cls=tuner_1_fixed, options=None):
-
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
+def main(capture_config: dict, top_block_cls=tuner_1_fixed, options=None):
 
     tb = top_block_cls(capture_config)
-
-    tb.start()
-
-    tb.show()
 
     def sig_handler(sig=None, frame=None):
         tb.stop()
         tb.wait()
 
-        Qt.QApplication.quit()
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    timer = Qt.QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)
+    tb.start()
 
-    qapp.exec_()
+    tb.wait()
 
-if __name__ == '__main__':
-    main()
