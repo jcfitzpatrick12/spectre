b65303099ea8f7a79472255e093e4f0d67d03e7e
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-05-08 20:28:53 +0100

    chore: simplify file_fix script

--- host/scripts/file_fix.sh
+++ host/scripts/file_fix.sh
@@ -6,29 +6,8 @@
 # this script detects the architecture and changes the suffix directory dynamically
 ########################
 
-
-# Function to determine architecture-specific directory suffix
-get_lib_dir_suffix() {
-    case "$(uname -m)" in
-        x86_64)
-            echo "x86_64-linux-gnu"
-            ;;
-        aarch64)
-            echo "aarch64-linux-gnu"
-            ;;
-        *) 
-            echo "unsupported-architecture"
-            echo "Unsupported architecture. Exiting script."
-            exit 1
-            ;;
-    esac
-}
-
-# Define architecture-specific part of the directory name
-lib_dir_suffix=$(get_lib_dir_suffix)
-
 # Define source and destination directories
-source_dir="/usr/local/lib/${lib_dir_suffix}"
+source_dir="/usr/local/lib/$(uname -m)-linux-gnu"
 dest_dir="/usr/lib/${lib_dir_suffix}"
 
 # Move files
