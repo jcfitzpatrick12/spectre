8db5dd449609a5e873f2f8613a7ec71054ae801b
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-05-15 21:02:34 +0100

    feat: initial commit for time and frequency slicing

--- spectre/spectrogram/PanelStacker.py
+++ spectre/spectrogram/PanelStacker.py
@@ -3,6 +3,7 @@ from matplotlib.figure import Figure
 from datetime import datetime
 
 from spectre.spectrogram.Panels import Panels
+from spectre.utils import array_helpers
  
 
 class PanelStacker(Panels):  
@@ -12,31 +13,55 @@ class PanelStacker(Panels):
     def create_figure(self, fig: Figure, panel_types: str):
             # Create a figure with subplots for plots and colorbars
             axs = fig.subplots(len(panel_types), 2, 
-                            gridspec_kw={'width_ratios': [3, 0.1], 'wspace': 0.05},
+                            gridspec_kw={'width_ratios': [3, 0.1], 
+                                         'wspace': 0.05,
+                                         'hspace': 0.3,
+                                         },
                             squeeze=False)
 
             # Store the first axis to link subsequent axes
             first_ax = None
 
-            # Iterate over the plot types and their respective axes
-            for i, (ax, cax) in enumerate(zip(axs[:, 0], axs[:, 1])):
-                # Turn the colorbar axis off initially; it will be turned on if needed
+            # if the user has requested "slices_at_times" we consider this first
+            # this is handled differently since it does not share the time axes with the other panels
+            # and does not have units of time on the x-axis
+            if "frequency_slices" in panel_types:
+                # set the panel type explicitly in this case
+                panel_type = "frequency_slices"
+                # move this to the front
+                panel_types = array_helpers.move_to_front(panel_types, "frequency_slices")
+                # Get the plotting function
+                plot_method = self.get_plot_method(panel_type)
+                # ax and cax will be the first panels
+                ax, cax = axs[0, 0], axs[0, 1]
+                # Call the plotting function with its specific kwargs
+                plot_method(ax=ax, cax=cax)
+                # Turn the colorbar axis off
                 cax.axis('off')
 
+
+            # Iterate over the plot types with Time on the x-axis, and plot
+            for i, (ax, cax) in enumerate(zip(axs[:, 0], axs[:, 1])):
+                panel_type = panel_types[i]
+
+                if panel_type == "frequency_slices":
+                    continue
+
+                # Turn the colorbar axis off initially; it will be turned on if needed
+                cax.axis('off')
                 # Get the plotting function
-                plot_method = self.get_plot_method(panel_types[i])
+                plot_method = self.get_plot_method(panel_type)
 
                 # Call the plotting function with its specific kwargs
                 plot_method(ax=ax, cax=cax)
 
-                # Manage x-axis visibility and linking
                 if first_ax is not None:
                     ax.sharex(first_ax)  # Link x-axis with the first axis
                 else:
                     first_ax = ax  # Set the first axis
 
-                if i == len(panel_types) - 1:
 
+                if i == len(panel_types) - 1:
                     if self.time_type == "datetimes":
                         ax.set_xlabel(f'Time [UTC]', size=self.fsize_head)
 
@@ -45,11 +70,12 @@ class PanelStacker(Panels):
                     
                     else:
                         raise ValueError(f"Must set a valid time type. Received {self.time_type}, expected one of {self.valid_time_types}.")
+
                 else:
                     ax.tick_params(labelbottom=False)  # Hide x-tick labels for all but the last ax
 
-            # Align all x-axes and labels
-            plt.subplots_adjust(hspace=0.05)  # Adjust horizontal space to zero
+            # # Align all x-axes and labels
+            # plt.subplots_adjust()  # Adjust horizontal space to zero
 
 
             
