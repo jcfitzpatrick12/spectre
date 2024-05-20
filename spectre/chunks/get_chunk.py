a043f625a604796302123dab4cf4179d6eb1bcde
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-05-20 21:35:29 +0100

    feat: allow non-native chunks (support for callisto data)

--- spectre/chunks/get_chunk.py
+++ spectre/chunks/get_chunk.py
@@ -15,7 +15,12 @@ def get_chunk(chunk_key: str) -> BaseChunk:
     return chunk
 
 def get_chunk_from_tag(tag: str) -> BaseChunk:
-    capture_config_handler = CaptureConfigHandler(tag)
-    capture_config = capture_config_handler.load_as_dict()
-    chunk_key = capture_config.get('chunk_key', None)
+    # if we are dealing with a callisto chunk, the chunk key is equal to the tag
+    if "callisto" in tag:
+        chunk_key = "callisto"
+    # otherwise, we fetch the chunk key from the capture config
+    else:
+        capture_config_handler = CaptureConfigHandler(tag)
+        capture_config = capture_config_handler.load_as_dict()
+        chunk_key = capture_config.get('chunk_key', None)
     return get_chunk(chunk_key)
