41bd36d9cb431598c23d77096a8f9785968e7b2b
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-05-21 22:06:25 +0100

    chore: simplify comparison stack

--- spectre/spectrogram/ComparisonStack.py
+++ spectre/spectrogram/ComparisonStack.py
@@ -8,46 +8,64 @@ from spectre.utils import array_helpers
  
 
 class ComparisonStack:  
-    def compare(self, fig: Figure, S0, S1, **kwargs):
+    def compare_with_callisto(self, fig: Figure, S, callisto_S, **kwargs):
         # Create a figure with subplots for plots and colorbars
         # order of comparison plots:
+
         ###
         # 0 - frequency slices
         # 1 - time slices
-        # 2 - integrated power comparison
-        # 3 - S0 spectrogram
-        # 4 - S1 spectrogram
+        # 2 - integrated frequency comparison
+        # 3 - S spectrogram
+        # 4 - callisto_S spectrogram
         ###
+
+        if "callisto" not in callisto_S.tag:
+            raise ValueError(f"Expected comparison with a callisto spectrogram! Received an unexpected tag {callisto_S.tag}")
+        
         axs = fig.subplots(5, 2, 
-                        gridspec_kw={'width_ratios': [3, 0.1], 
+                        gridspec_kw={'width_ratios': [2.5, 0.1], 
                                         'wspace': 0.05,
                                         'hspace': 0.4,
                                         },
                         squeeze=False)
         
-        kwargs["fsize_head"] = 14
-        S0_kwargs = kwargs.copy()
-        S0_kwargs['slice_cmap'] = cm.summer
-        
-        S1_kwargs = kwargs.copy()
-        S1_kwargs['slice_cmap'] = cm.winter
+        kwargs['normalise_line_plots'] = True
+        kwargs['add_tag_to_annotation'] = True
 
-        S0_panels = Panels(S0, **S0_kwargs)
-        S1_panels = Panels(S1, **S1_kwargs)
+        S_kwargs = kwargs.copy()
+        S_kwargs['slice_color'] = 'fuchsia'
+        S_kwargs['dB_vmin']=-1
+        S_kwargs['dB_vmax']=4
+        
+        callisto_S_kwargs = kwargs.copy()
+        callisto_S_kwargs['slice_color'] = 'dodgerblue'
+        callisto_S_kwargs['dB_vmin']=-1
+        callisto_S_kwargs['dB_vmax']=14
+        callisto_S_kwargs['annotation_vertical_offset'] = 0.8
+
+        S_panels = Panels(S, **S_kwargs)
+        callisto_S_panels = Panels(callisto_S, **callisto_S_kwargs)
 
         # Turn the colorbar axis off
         axs[0,1].axis('off')
-        S0_panels.frequency_slices(ax=axs[0,0], cax=axs[0,1])
-        S1_panels.frequency_slices(ax=axs[0,0], cax=axs[0,1])
+        # axs[0,0].tick_params(labelleft=False)
+        # axs[0,0].tick_params(labelbottom=False) 
+        S_panels.frequency_slice(ax=axs[0,0], cax=axs[0,1])
+        callisto_S_panels.frequency_slice(ax=axs[0,0], cax=axs[0,1])
 
         axs[1,1].axis('off')
-        S0_panels.time_slices(ax=axs[1,0], cax=axs[1,1])
-        S1_panels.time_slices(ax=axs[1,0], cax=axs[1,1])
+        axs[1,0].tick_params(labelbottom=False) 
+        # axs[1,0].tick_params(labelleft=False) 
+        S_panels.time_slice(ax=axs[1,0], cax=axs[1,1])
+        callisto_S_panels.time_slice(ax=axs[1,0], cax=axs[1,1])
         spectrogram_type = kwargs.get("spectrogram_type", "raw")
         
         axs[2,1].axis('off')
-        S0_panels.integrated_power(ax=axs[2,0], cax=axs[2,1])
-        S1_panels.integrated_power(ax=axs[2,0], cax=axs[2,1])
+        axs[2,0].tick_params(labelbottom=False) 
+        # axs[2,0].tick_params(labelleft=False)
+        S_panels.integrate_over_frequency(ax=axs[2,0], cax=axs[2,1])
+        callisto_S_panels.integrate_over_frequency(ax=axs[2,0], cax=axs[2,1])
         axs[2,0].sharex(axs[1,0])  # Link x-axis with the first axis
         
         expected_spectrogram_types = ["dBb", "rawlog", "raw"]
@@ -55,25 +73,18 @@ class ComparisonStack:
             raise ValueError(f"Received unexpected spectrogram_type: {spectrogram_type}. Expected one of {expected_spectrogram_types}.")
         
         axs[3,1].axis('off')
-        S0_spectrogram_plot_method = S0_panels.get_plot_method(spectrogram_type)
-        S0_spectrogram_plot_method(axs[3,0], axs[3,1])
+        axs[3,0].tick_params(labelbottom=False) 
+        # axs[3,0].tick_params(labelleft=False)
+        S_spectrogram_plot_method = S_panels.get_plot_method(spectrogram_type)
+        S_spectrogram_plot_method(axs[3,0], axs[3,1])
         axs[3,0].sharex(axs[1,0])
 
         axs[4,1].axis('off')
-        S1_spectrogram_plot_method = S1_panels.get_plot_method(spectrogram_type)
-        S1_spectrogram_plot_method(axs[4,0], axs[4,1])
+        callisto_S_spectrogram_plot_method = callisto_S_panels.get_plot_method(spectrogram_type)
+        callisto_S_spectrogram_plot_method(axs[4,0], axs[4,1])
         axs[4,0].sharex(axs[1,0])
 
-        time_type = kwargs.get("time_type", "time_seconds")
-        if time_type == "time_seconds":
-            axs[4,0].set_xlabel(f'Time [s]', size = S1_panels.fsize_head) 
-
-        elif time_type == "datetimes":
-            axs[4,0].set_xlabel(f'Time [UTC]', size = S1_panels.fsize_head)
-        
-        else:
-            raise ValueError(f"Must set a valid time type. Received {self.time_type}, expected one of {self.valid_time_types}.")
-        
-
+        axs[4,0].set_xlabel(f'Time [UTC]', size = callisto_S_panels.fsize_head)
+    
         fig.align_ylabels() 
         return
