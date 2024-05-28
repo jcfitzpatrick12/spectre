cf967ad58b203a73f7c14dba8b7bcec99dd83c91
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-05-28 22:27:30 +0000

    feat: initial commit for cosine_signal_test_1 validation functions

--- spectre/receivers/library/Test/mounts/CaptureConfigMount.py
+++ spectre/receivers/library/Test/mounts/CaptureConfigMount.py
@@ -45,6 +45,93 @@ class CaptureConfigMount(BaseCaptureConfigMount):
 
 
     def cosine_signal_test_1_validator(self, capture_config: dict) -> None:
+        # unpack the capture config
+        samp_rate = capture_config.get("samp_rate")
+        frequency = capture_config.get("frequency")
+        amplitude = capture_config.get("amplitude")
+        chunk_size = capture_config.get("chunk_size")
+        window_type = capture_config.get("window_type")
+        Nx = capture_config.get("Nx")
+        STFFT_kwargs = capture_config.get("STFFT_kwargs")
+        chunk_key = capture_config.get("chunk_key")
+        event_handler_key = capture_config.get("event_handler_key")
+        watch_extension = capture_config.get("watch_extension")
+        integration_time = capture_config.get("integration_time")
+
+
+        if integration_time != 0:
+            raise ValueError(f"Integration time must be zero. Received {integration_time}")
+
+
+        if watch_extension != ".bin":
+            raise ValueError(f"watch_extension must be \".bin\". Received {watch_extension}")
+
+
+        if chunk_key != "default":
+            raise ValueError(f"chunk_key must be \"default\". Received {chunk_key}")
+
+        
+        if event_handler_key != "default":
+            raise ValueError(f"event_handler_key must be \"default\". Received {event_handler_key}")
+
+
+        # checking that the chunk_size is greater than the period of the underlying signal
+        T = 1 / frequency
+        # chunk_size must be more than one period  
+        if chunk_size < T:
+            raise ValueError("chunk_size must be equal than or greater to the period of the underlying signal.")
+        
+
+        # check that the sample rate is an integer multiple of the underlying signal frequency
+        if samp_rate % frequency != 0:
+            raise ValueError("samp_rate must be some integer multiple of frequency.")
+
+        # checking that the sample rate is above the Nyquist rate
+        # a is also the number of samples taken over one cycle of the underlying signal
+        a = int(samp_rate/frequency)
+        if a < 2:
+            raise ValueError(f"The ratio samp_rate/frequency must be a natural number greater than two.  Received {a}")
+        
+        # ensuring the window type is rectangular
+        if window_type != "boxcar":
+            raise ValueError(f"Window type must be \"box\". Received {window_type}")
+        
+        # ensuring that the hop is specified as a keyword argument
+        if set(STFFT_kwargs.keys()) != {"hop"}:
+            raise KeyError(f"Only allowed kwarg is STFFT_kwargs is \"hop\". Received {STFFT_kwargs.keys()}")
+        
+        # checking that hop is of integer type
+        hop = STFFT_kwargs.get("hop")
+        if type(hop) != int:
+            raise TypeError(f"hop must an integer. Received {hop}")
+        
+        # checking that hop is an integer multiple of the ratio (samp_rate/frequency)
+        if hop % a != 0:
+            raise ValueError(f"hop must be an integer multiple of samp_rate/frequency. Received {hop}")
+
+        # checking that we are "hopping" over at least one period of the underlying signal
+        b = int(hop / a)
+        if b < 1:
+            raise ValueError("The ratio hop/Np, where Np is the number of samples in one cycle according to the sample rate, must be a positive natural number")
+        
+        # analytical requirement
+        # if p is the number of sampled cycles, we can find that p = Nx / a
+        # the number of sampled cycles must be a positive natural number.
+        p = Nx / a
+        if Nx % a != 0:
+            raise ValueError(f"The number of sampled cycles must be a positive natural number. Computed that p={p}")
+
+        p = int(p)
+        if p < 1:
+            raise ValueError(f"The number of sampled cycles must be a positive natural number. Computed that p={p}")
+        
+        if p % 2 != 0 :
+            raise ValueError(f"The number of sampled cycles must be an even positive natural number. Computed that p={p}")
+
+        # the window size must be greater than the hop
+        if Nx < hop:
+            raise ValueError("Window size (# samples in the window), must be greater than the hop.")
+        
         return
     
 
