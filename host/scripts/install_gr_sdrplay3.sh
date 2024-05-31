1ab04196c96cd0deab59572f2b0cb1122e2826ea
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-05-31 21:53:19 +0100

    fix: define a stable version of gr-sdrplay3 repo with 3.14.0 of the SDRPlay RSP API

--- host/scripts/install_gr_sdrplay3.sh
+++ host/scripts/install_gr_sdrplay3.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/fventuri/gr-sdrplay3.git
 #cd into the cloned repo
 cd gr-sdrplay3
+# checkout a stable version
+git checkout v3.11.0.4
 #built the OOT module
 mkdir build && cd build && cmake .. && make
 sudo make install
