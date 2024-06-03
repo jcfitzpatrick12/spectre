6895c06415477f046d188bdb6c491d356bb62889
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-06-03 20:14:36 +0000

    chore: offload tag validations to JsonConfigHandler

--- spectre/json_config/FitsConfigHandler.py
+++ spectre/json_config/FitsConfigHandler.py
@@ -5,16 +5,7 @@ from spectre.utils import dict_helpers
 
 class FitsConfigHandler(JsonConfigHandler):
     def __init__(self, tag: str):
-        if tag == None:
-            raise ValueError(f'tag cannot be None. Received {tag}.')
-        
-
-        if "_" in tag:
-            raise ValueError(f"Tags cannot contain an underscore. Received {tag}.")
-    
-    
-        name = f"fits_config_{tag}"
-        super().__init__(name)
+        super().__init__("fits", tag)
 
 
     def get_template(self,) -> dict:
