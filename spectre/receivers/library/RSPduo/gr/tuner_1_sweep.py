89050d4fca5a22016ef2425a49d45a263e0827aa
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-06-28 11:54:48 +0000

    fix: missing arguments in top block instantiation

--- spectre/receivers/library/RSPduo/gr/tuner_1_sweep.py
+++ spectre/receivers/library/RSPduo/gr/tuner_1_sweep.py
@@ -89,7 +89,7 @@ class tuner_1_sweep(gr.top_block):
 
 
 def main(capture_config: dict, top_block_cls=tuner_1_sweep, options=None):
-    tb = top_block_cls()
+    tb = top_block_cls(capture_config)
 
     def sig_handler(sig=None, frame=None):
         tb.stop()
