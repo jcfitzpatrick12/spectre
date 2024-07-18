4d39ae8c54a168955251379de6f712bf883debc0
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-07-18 18:08:05 +0100

    feat: mode enforced internally entirely by mode setter

--- spectre/receivers/BaseReceiver.py
+++ spectre/receivers/BaseReceiver.py
@@ -37,51 +37,47 @@ class BaseReceiver(ABC):
         self.mode = mode
 
 
-    def get_capture_method(self, mode: str) -> Callable:
-        capture_method = self.capture_methods.get(mode)
+    def get_capture_method(self) -> Callable:
+        capture_method = self.capture_methods.get(self.mode)
         if capture_method is None:
-            raise ValueError(f"Invalid mode '{mode}'. Valid modes are: {self.valid_modes}")
+            raise ValueError(f"Invalid mode '{self.mode}'. Valid modes are: {self.valid_modes}")
         return capture_method
 
 
-    def get_validator(self, mode: str) -> Callable:
-        validator = self.validators.get(mode)
+    def get_validator(self) -> Callable:
+        validator = self.validators.get(self.mode)
         if validator is None:
-            raise ValueError(f"Invalid mode '{mode}'. Valid modes are: {self.valid_modes}")
+            raise ValueError(f"Invalid mode '{self.mode}'. Valid modes are: {self.valid_modes}")
         return validator
 
 
-    def get_template(self, mode: str) -> dict:
-        template = self.templates.get(mode)
+    def get_template(self) -> dict:
+        template = self.templates.get(self.mode)
         if template is None:
-            raise ValueError(f"Invalid mode '{mode}'. Valid modes are: {self.valid_modes}")
+            raise ValueError(f"Invalid mode '{self.mode}'. Valid modes are: {self.valid_modes}")
         return template
-    
-
-    def get_mode(self) -> str:
-        return self.mode
 
 
     def validate(self, capture_config: dict) -> None:
-        validator = self.get_validator(self.mode)
+        validator = self.get_validator()
         validator(capture_config)
 
 
     def start_capture(self, tags: list) -> None:
         capture_configs = [self.load_capture_config(tag) for tag in tags]
-        capture_method = self.get_capture_method(self.mode)
+        capture_method = self.get_capture_method()
         capture_method(capture_configs)
 
 
     def save_params_as_capture_config(self, params: list, tag: str, doublecheck_overwrite: bool = True) -> None:
         string_valued_dict = dict_helpers.params_list_to_string_valued_dict(params)
-        template = self.get_template(self.mode)
+        template = self.get_template()
         capture_config = dict_helpers.convert_types(string_valued_dict, template)
         self.save_capture_config(tag, capture_config, doublecheck_overwrite)
 
 
     def save_capture_config(self, tag: str, capture_config: dict, doublecheck_overwrite: bool = True) -> None:
-        template = self.get_template(self.mode)
+        template = self.get_template()
         dict_helpers.validate_keys(capture_config, template, ignore_keys=['receiver', 'mode', 'tag'])
         dict_helpers.validate_types(capture_config, template, ignore_keys=['receiver', 'mode', 'tag'])
         self.validate(capture_config)
@@ -107,7 +103,7 @@ class BaseReceiver(ABC):
 
     def template_to_command(self, tag: str, as_string: bool = False) -> str:
         command_as_list = ["spectre", "create", "capture-config", "-r", self.receiver_name, "-m", self.mode, "-t", tag]
-        template = self.get_template(self.mode)
+        template = self.get_template()
         for key, value in template.items():
             command_as_list.extend(["-p", f"{key}={value.__name__}"])
 
