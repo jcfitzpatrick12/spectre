ae8ddf219a561b43e0dc09c94fece3411c1c746d
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-07-22 07:51:33 +0100

    chore: refactor s.t. ChunkExt inherits from BaseChunk

--- spectre/chunks/ChunkExt.py
+++ spectre/chunks/ChunkExt.py
@@ -8,16 +8,14 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 from spectre.utils import datetime_helpers, file_helpers
+from spectre.chunks.BaseChunk import BaseChunk
 from cfg import CONFIG
 
-class ChunkExt(ABC):
+class ChunkExt(ABC, BaseChunk):
     def __init__(self, chunk_start_time: str, tag: str, ext: str):
-        self.chunk_start_time = chunk_start_time
-        self.tag = tag
+        super().__init__(chunk_start_time, tag)
         self.ext = ext
         self.file = f"{self.chunk_start_time}_{tag}.{self.ext}"
-        self.chunk_dir = datetime_helpers.build_chunks_dir(self.chunk_start_time)
-        self.chunk_start_datetime = datetime.strptime(chunk_start_time, CONFIG.default_time_format)
 
 
     @abstractmethod
