fb2e07c3682db6eb2012dc201401eea6151323dc
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-07-22 19:29:34 +0100

    feat: initial commit for sweep chunk

---
+++ spectre/chunks/library/sweep/HdrChunk.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: © 2024 Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
+# This file is part of SPECTRE
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import numpy as np
+from typing import Tuple
+
+from spectre.chunks.ExtChunk import ExtChunk
+
+class HdrChunk(ExtChunk):
+    def __init__(self, chunk_start_time: str, tag: str):
+        super().__init__(chunk_start_time, tag, ".hdr")
+    
+    # ! TO BE IMPLEMENTED ! #
+    def read(self) -> int:
+        return 0
