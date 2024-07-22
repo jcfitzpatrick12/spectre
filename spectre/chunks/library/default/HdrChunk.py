e5566ed854010373c4a7c5bbd1bfeafd7e964e47
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-07-22 21:54:58 +0100

    chore: simplify modulurisation

--- spectre/chunks/library/default/HdrChunk.py
+++ spectre/chunks/library/default/HdrChunk.py
@@ -11,10 +11,10 @@ class HdrChunk(ExtChunk):
     def __init__(self, chunk_start_time: str, tag: str):
         super().__init__(chunk_start_time, tag, ".hdr")
 
+
     def read(self) -> int:
         try:
             hdr_contents = self._read_file_contents()
-            self._validate_hdr_contents(hdr_contents)
             return self._get_millisecond_correction(hdr_contents)
 
         except FileNotFoundError as e:
@@ -27,13 +27,10 @@ class HdrChunk(ExtChunk):
             return np.fromfile(fh, dtype=np.float32)
 
 
-    def _validate_hdr_contents(self, hdr_contents: np.ndarray) -> None:
+    def _get_millisecond_correction(self, hdr_contents: np.ndarray) -> int:
         # Validates that the header file contains exactly one element 
         if len(hdr_contents) != 1:
             raise ValueError(f"Only expected one integer in the header, but received header contents: {hdr_contents}")
-
-
-    def _get_millisecond_correction(self, hdr_contents: np.ndarray) -> int:
         # Extracts and returns the millisecond correction from the file contents 
         millisecond_correction_as_float = float(hdr_contents[0])
         if millisecond_correction_as_float.is_integer():
