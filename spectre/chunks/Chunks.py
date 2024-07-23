87f83d962f6819c86a99eb5940ce4e1be4c570a5
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-07-23 22:51:20 +0100

    feat: initial commit chunks refactor

--- spectre/chunks/Chunks.py
+++ spectre/chunks/Chunks.py
@@ -9,10 +9,11 @@ from datetime import datetime
 from typing import Tuple
 
 from spectre.chunks.factory import get_chunk_from_tag
+from spectre.chunks.BaseChunk import BaseChunk
 from spectre.utils import dir_helpers, datetime_helpers
-from cfg import CONFIG
 from spectre.spectrogram.Spectrogram import Spectrogram
 from spectre.spectrogram import factory
+from cfg import CONFIG
 
 class Chunks:
     def __init__(self, 
@@ -33,147 +34,114 @@ class Chunks:
                                                                day=day)
 
         self.Chunk = get_chunk_from_tag(tag)
-        self.dict = self.build_dict()
+        self._set_chunk_map()
 
 
-    def build_dict(self) -> None:
-        chunks_dict = OrderedDict()
+    def _set_chunk_map(self) -> None:
+        chunk_map = OrderedDict()
 
-        files = dir_helpers.list_all_files(self.chunks_dir)
+        ext_chunk_files = dir_helpers.list_all_files(self.chunks_dir)
         
-        if len(files) == 0:
+        if len(ext_chunk_files) == 0:
             raise FileNotFoundError(f"No chunks found at {self.chunks_dir}.")
         
-        for file in files:
-            file_name, ext = os.path.splitext(file)
+        for ext_chunk_file in ext_chunk_files:
+            file_name, _ = os.path.splitext(ext_chunk_file)
             try:
                 chunk_start_time, tag = file_name.split("_", 1)
             except ValueError as e:
                 print(f"Error while splitting {file_name} at \"_\". Received {e}")
             # only consider chunks with the specified tag
             if tag == self.tag:
-                chunks_dict[chunk_start_time] = self.Chunk(chunk_start_time, tag)
+                chunk_map[chunk_start_time] = self.Chunk(chunk_start_time, tag)
         
-        # sort the dictionary and set the attribute
-        return OrderedDict(sorted(chunks_dict.items()))
+        # sort the dictionary by keys in ascending chronological order
+        chunk_map = OrderedDict(sorted(chunk_map.items()))
+        
+        # finally, set the attribute
+        self.chunk_map = chunk_map
+        return
 
 
-    def get_chunk_by_index(self, index: int):
-            if not self.dict:  # Check if the dictionary is empty
-                print("The chunk dictionary is empty. Returning None.")
-                return None
+    def get_chunk_start_time_list(self):
+        return list(self.chunk_map.keys()) 
+    
 
-            # Normalize index to be within the bounds using modulo
-            num_chunks = len(self.dict)  # Number of items in the dictionary
-            index = index % num_chunks  # Use modulo to make the index wrap around
+    def get_chunk_list(self):
+        return list(self.chunk_map.values())
+    
 
-            for i, chunk in enumerate(self.dict.values()):
-                if i == index:
-                    return chunk
+    def get_chunk_by_chunk_start_time(self, chunk_start_time: str) -> BaseChunk:
+        chunk = self.chunk_map.get(chunk_start_time)
+        if chunk is None:
+            raise KeyError(f"Chunk with chunk start time {chunk_start_time} could not be found within {self.chunks_dir}")
+        return chunk
+
+
+    def get_chunk_by_item_index(self, item_index: int) -> BaseChunk:
+            num_chunks = len(self.chunk_map) 
+            index = index % num_chunks  # Use modulo to make the index wrap around. Allows the user to iterate over all the chunks via index cyclically.
             
-            # Although logically this part should never be reached because of the modulo operation
+            chunk_list = self.get_chunk_list()
+            for i, chunk in enumerate(chunk_list):
+                if i == item_index:
+                    return chunk
+        
+            # Return Although logically this part should never be reached because of the modulo operation
             warnings.warn(f"Unexpected error! Index {index} out of bounds for maximum index {num_chunks-1}.", RuntimeWarning)
             return None
 
 
-    def get_index_by_chunk(self, chunk_to_match) -> int:
-        chunk_start_time_to_match = chunk_to_match.chunk_start_time
-
-        for index, chunk in enumerate(self.dict.values()):
-            if chunk.chunk_start_time == chunk_start_time_to_match:
-                return index
-
-        raise ValueError(f"No matching chunk found with chunk_start_time {chunk_start_time_to_match}.")
+    def get_item_index_by_chunk(self, chunk_to_match: BaseChunk) -> int:
+        chunk_list = self.get_chunk_list()
+        for i, chunk in enumerate(chunk_list):
+            if chunk.chunk_start_time == chunk_to_match.chunk_start_time:
+                return i
+        raise ValueError(f"No matching chunk found with chunk_start_time {chunk_to_match.chunk_start_time}.")
     
 
-    def find_nearest_chunk(self, requested_chunk_start_time: str):
-        try:
-            requested_chunk_start_time = datetime.strptime(requested_chunk_start_time, CONFIG.default_time_format)
-        except ValueError as e:
-            raise ValueError(f"Invalid date format for current chunk. Expected {CONFIG.default_time_format}, but got {requested_chunk_start_time}.")
-        
-        closest_chunk = None
-        min_time_diff = None
 
-        for chunk in self.dict.values():
-            # extract the chunk_start_time for the current chunk 
-            chunk_start_time = chunk.chunk_start_time
-            
-            # Calculate the absolute time difference
-            time_diff = abs(requested_chunk_start_time - chunk_start_time)
+    def build_spectrogram_from_range(self, start_time_str: str, end_time_str: str) -> Spectrogram:
+        start_time = datetime.strptime(start_time_str, CONFIG.default_time_format)
+        end_time = datetime.strptime(end_time_str, CONFIG.default_time_format)
 
-            # Determine if this is the closest chunk so far
-            if (min_time_diff is None) or (time_diff < min_time_diff):
-                closest_chunk = chunk
-                min_time_diff = time_diff
+        if start_time.day != end_time.day:
+            warnings.warn("Warning! Joining spectrograms over more than one day.", RuntimeWarning)
 
-        if closest_chunk is None:
-            raise ValueError(f"Unexpected error, closest chunk could not be evaluated. Found {closest_chunk}.")
+        target_day = start_time.day
+        chunk_intervals = self.get_upper_bound_chunk_intervals()
+        spectrograms = []
 
-        return closest_chunk
-    
-
-    def build_spectrogram_from_range(self, requested_start_str: str, requested_end_str: str) -> Spectrogram:
-        requested_start_datetime = datetime.strptime(requested_start_str, CONFIG.default_time_format)
-        requested_end_datetime = datetime.strptime(requested_end_str, CONFIG.default_time_format)
-
-
-        if requested_start_datetime.day != requested_end_datetime.day:
-            warnings.warn(f"Warning! Joining spectrograms over a time interval of more than one day.", RuntimeWarning)
-
-        # can now safely set the day requested
-        requested_day = requested_start_datetime.day
-
-        # get the upper bound chunk intervals map
-        upper_bound_chunk_intervals = self.get_upper_bound_chunk_intervals()
-
-        spectrogram_list = []
-        for chunk_start_time, chunk in self.dict.items():
-            # if the current chunk does not match the requested datetime, just continue
-            if chunk.chunk_start_datetime.day!=requested_day or not chunk.fits.exists():
+        for chunk_start_time, chunk in self.chunk_map.items():
+            if chunk.chunk_start_datetime.day != target_day or not chunk.fits.exists():
                 continue
-            
 
-            lower_bound_datetime = upper_bound_chunk_intervals[chunk_start_time][0]
-            upper_bound_datetime = upper_bound_chunk_intervals[chunk_start_time][1]
+            lower_bound, upper_bound = chunk_intervals[chunk_start_time]
 
-            if lower_bound_datetime < requested_end_datetime and upper_bound_datetime > requested_start_datetime:
-                S = chunk.fits.read()
-                S = factory.time_chop(S, requested_start_str, requested_end_str)
-                if S is None:
-                    raise ValueError(f"Could not time chop spectrogram while building from range.")
-                spectrogram_list.append(S)
+            if lower_bound < end_time and upper_bound > start_time:
+                spectrogram = chunk.fits.read()
+                chopped_spectrogram = factory.time_chop(spectrogram, start_time_str, end_time_str)
+                if chopped_spectrogram is None:
+                    raise ValueError("Error chopping spectrogram within the specified time range.")
+                spectrograms.append(chopped_spectrogram)
 
-        return factory.join_spectrograms(spectrogram_list)
+        return factory.join_spectrograms(spectrograms)
+
     
 
-    def get_upper_bound_chunk_intervals(self) -> dict[str, Tuple[datetime, datetime]]: 
-
-        chunk_list = list(self.dict.values())
-        num_chunks = len(chunk_list)
-
-         # # get the upper bound chunk intervals as a map s.t.
-        # # chunk_start_time -> tuple[t0, t1]
-        # where t0 is the current chunk start time as a datetime
-        # and t1 is the upper bound on the chunk end time as a datetime
-        # which we define to be the start of the next chunk
-        upper_bound_chunk_intervals = {}
+    def get_upper_bound_chunk_intervals(self) -> dict[str, Tuple[datetime, datetime]]:
+        chunk_list = self.get_chunk_list()
+        total_chunks = len(chunk_list)
+        chunk_intervals = {}
 
         for i, chunk in enumerate(chunk_list):
             if not chunk.fits.exists():
                 continue
-            
-            t0 = chunk.chunk_start_datetime
 
-            if i < num_chunks - 1:
-                next_chunk = chunk_list[i+1]
-                t1 = next_chunk.chunk_start_datetime
-            
-            # if we are at the last chunk, just explicately evaluate it
-            else:
-                datetimes = chunk.fits.get_datetimes()
-                t1 = datetimes[-1]
-            
-            upper_bound_chunk_intervals[chunk.chunk_start_time] = (t0, t1)
-        
-        return upper_bound_chunk_intervals
+            start_time = chunk.chunk_start_datetime
+            end_time = chunk_list[i + 1].chunk_start_datetime if i < total_chunks - 1 else chunk.fits.get_datetimes()[-1]
+
+            chunk_intervals[chunk.chunk_start_time] = (start_time, end_time)
+
+        return chunk_intervals
+
