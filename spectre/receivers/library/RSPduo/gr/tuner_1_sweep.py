3295ccb1d7155c6dc6f0053b11fed9c9c2385c55
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-08-22 17:26:26 +0100

    fix: temp hard-code

--- spectre/receivers/library/RSPduo/gr/tuner_1_sweep.py
+++ spectre/receivers/library/RSPduo/gr/tuner_1_sweep.py
@@ -44,14 +44,8 @@ class tuner_1_sweep(gr.top_block):
         #################################################
         # Blocks
         ##################################################
-        self.spectre_sweep_driver_0 = spectre.sweep_driver(min_freq, max_freq, freq_step, samp_rate, samples_per_step, 'freq')
-        self.spectre_batched_file_sink_0 = spectre.batched_file_sink(CONFIG.path_to_chunks_dir,
-                                                                     tag, 
-                                                                     chunk_size, 
-                                                                     samp_rate, 
-                                                                     True,
-                                                                     'freq',
-                                                                     start_freq)
+        self.spectre_sweep_driver_0 = spectre.sweep_driver(90000000, 100000000, 1000000, 1000000, 1000000, 'freq')
+        self.spectre_batched_file_sink_0 = spectre.batched_file_sink('/tmp/chunks', 'sweep-test', 3, 1000000, True, 'freq', 90000000)
         self.sdrplay3_rspduo_0 = sdrplay3.rspduo(
             '',
             rspduo_mode="Single Tuner",
@@ -62,32 +56,80 @@ class tuner_1_sweep(gr.top_block):
             ),
         )
         self.sdrplay3_rspduo_0.set_sample_rate(samp_rate, True)
-        self.sdrplay3_rspduo_0.set_center_freq(start_freq, True)
-        self.sdrplay3_rspduo_0.set_bandwidth(bandwidth)
+        self.sdrplay3_rspduo_0.set_center_freq(90e6, True)
+        self.sdrplay3_rspduo_0.set_bandwidth(1536e3)
         self.sdrplay3_rspduo_0.set_antenna("Tuner 1 50 ohm")
         self.sdrplay3_rspduo_0.set_gain_mode(False)
-        self.sdrplay3_rspduo_0.set_gain(IF_gain, 'IF', True)
-        self.sdrplay3_rspduo_0.set_gain(RF_gain, 'RF', True)
+        self.sdrplay3_rspduo_0.set_gain(-(40), 'IF', True)
+        self.sdrplay3_rspduo_0.set_gain(-(0), 'RF', True)
         self.sdrplay3_rspduo_0.set_freq_corr(0)
         self.sdrplay3_rspduo_0.set_dc_offset_mode(False)
         self.sdrplay3_rspduo_0.set_iq_balance_mode(False)
         self.sdrplay3_rspduo_0.set_agc_setpoint(-30)
         self.sdrplay3_rspduo_0.set_rf_notch_filter(False)
-        self.sdrplay3_rspduo_0.set_dab_notch_filter(False)
+        self.sdrplay3_rspduo_0.set_dab_notch_filter(True)
         self.sdrplay3_rspduo_0.set_am_notch_filter(False)
         self.sdrplay3_rspduo_0.set_biasT(False)
         self.sdrplay3_rspduo_0.set_stream_tags(True)
         self.sdrplay3_rspduo_0.set_debug_mode(False)
         self.sdrplay3_rspduo_0.set_sample_sequence_gaps_check(False)
         self.sdrplay3_rspduo_0.set_show_gain_changes(False)
+        self.blocks_tag_debug_0 = blocks.tag_debug(gr.sizeof_gr_complex*1, 'freq', "freq")
+        self.blocks_tag_debug_0.set_display(True)
 
 
         ##################################################
         # Connections
         ##################################################
         self.msg_connect((self.spectre_sweep_driver_0, 'freq'), (self.sdrplay3_rspduo_0, 'freq'))
+        self.connect((self.sdrplay3_rspduo_0, 0), (self.blocks_tag_debug_0, 0))
         self.connect((self.sdrplay3_rspduo_0, 0), (self.spectre_batched_file_sink_0, 0))
         self.connect((self.sdrplay3_rspduo_0, 0), (self.spectre_sweep_driver_0, 0))
+
+        # self.spectre_sweep_driver_0 = spectre.sweep_driver(min_freq, max_freq, freq_step, samp_rate, samples_per_step, 'freq')
+        # self.spectre_batched_file_sink_0 = spectre.batched_file_sink(CONFIG.path_to_chunks_dir,
+        #                                                              tag, 
+        #                                                              chunk_size, 
+        #                                                              samp_rate, 
+        #                                                              True,
+        #                                                              'freq',
+        #                                                              start_freq)
+        # self.sdrplay3_rspduo_0 = sdrplay3.rspduo(
+        #     '',
+        #     rspduo_mode="Single Tuner",
+        #     antenna="Tuner 1 50 ohm",
+        #     stream_args=sdrplay3.stream_args(
+        #         output_type='fc32',
+        #         channels_size=1
+        #     ),
+        # )
+        # self.sdrplay3_rspduo_0.set_sample_rate(samp_rate, True)
+        # self.sdrplay3_rspduo_0.set_center_freq(start_freq, True)
+        # self.sdrplay3_rspduo_0.set_bandwidth(bandwidth)
+        # self.sdrplay3_rspduo_0.set_antenna("Tuner 1 50 ohm")
+        # self.sdrplay3_rspduo_0.set_gain_mode(False)
+        # self.sdrplay3_rspduo_0.set_gain(IF_gain, 'IF', True)
+        # self.sdrplay3_rspduo_0.set_gain(RF_gain, 'RF', True)
+        # self.sdrplay3_rspduo_0.set_freq_corr(0)
+        # self.sdrplay3_rspduo_0.set_dc_offset_mode(False)
+        # self.sdrplay3_rspduo_0.set_iq_balance_mode(False)
+        # self.sdrplay3_rspduo_0.set_agc_setpoint(-30)
+        # self.sdrplay3_rspduo_0.set_rf_notch_filter(False)
+        # self.sdrplay3_rspduo_0.set_dab_notch_filter(False)
+        # self.sdrplay3_rspduo_0.set_am_notch_filter(False)
+        # self.sdrplay3_rspduo_0.set_biasT(False)
+        # self.sdrplay3_rspduo_0.set_stream_tags(True)
+        # self.sdrplay3_rspduo_0.set_debug_mode(False)
+        # self.sdrplay3_rspduo_0.set_sample_sequence_gaps_check(False)
+        # self.sdrplay3_rspduo_0.set_show_gain_changes(False)
+
+
+        # ##################################################
+        # # Connections
+        # ##################################################
+        # self.msg_connect((self.spectre_sweep_driver_0, 'freq'), (self.sdrplay3_rspduo_0, 'freq'))
+        # self.connect((self.sdrplay3_rspduo_0, 0), (self.spectre_batched_file_sink_0, 0))
+        # self.connect((self.sdrplay3_rspduo_0, 0), (self.spectre_sweep_driver_0, 0))
 
 
 def main(capture_config: dict, top_block_cls=tuner_1_sweep, options=None):
