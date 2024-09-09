5f9894bea51741b6fa98637365040eb654fe2b4d
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-09-09 19:36:17 +0100

    fix: add duration to CLI arguments

--- host/cli/tools/capture/commands.py
+++ host/cli/tools/capture/commands.py
@@ -19,12 +19,18 @@ app = typer.Typer()
 def start_session(receiver_name: str = typer.Option(..., "--receiver", "-r", help="Specify the receiver name"),
                   mode: str = typer.Option(..., "--mode", "-m", help="Specify the mode for capture"),
                   tags: List[str] = typer.Option(..., "--tag", "-t", help="Specify the tags for the capture session."),
+                  seconds: int = typer.Option(0, "--seconds", help="Seconds component of the session duration."),
+                  minutes: int = typer.Option(0, "--minutes", help="Minutes component of the session duration."),
+                  hours: int = typer.Option(0, "--hours", help="Hours component of the session duration."),
                   force_restart: bool = typer.Option(False, "--force-restart", help="If a subprocess stops, terminate all subprocesses and restart the capture session..")
                  ) -> None:
 
     session.start_session(receiver_name,
                           mode,
                           tags,
+                          seconds = seconds,
+                          minutes = minutes,
+                          hours = hours,
                           force_restart = force_restart)
     return
 
