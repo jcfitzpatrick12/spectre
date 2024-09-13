8941f9ad8708a896153cea06b00e104c3c27ffb9
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-09-13 01:05:25 +0100

    fix: duplicate from in import

--- spectre/watchdog/BaseEventHandler.py
+++ spectre/watchdog/BaseEventHandler.py
@@ -9,7 +9,7 @@ from watchdog.events import FileSystemEventHandler
 from abc import ABC, abstractmethod
 
 from spectre.chunks.factory import get_chunk_from_tag
-from from spectre.file_handlers.json.CaptureConfigHandler import CaptureConfigHandler
+from spectre.file_handlers.json.CaptureConfigHandler import CaptureConfigHandler
 
 
 class BaseEventHandler(ABC, FileSystemEventHandler):
