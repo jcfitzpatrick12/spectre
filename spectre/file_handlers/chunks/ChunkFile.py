05b76afc92878355e1962f0b162da6e8229985d1
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-09-18 22:00:13 +0100

    fix: redundant type annotations

--- spectre/file_handlers/chunks/ChunkFile.py
+++ spectre/file_handlers/chunks/ChunkFile.py
@@ -13,6 +13,6 @@ class ChunkFile(BaseFileHandler):
     def __init__(self, chunk_parent_path: str, chunk_name: str, extension: str):
         self.extension = extension
         self.chunk_start_time, self.tag = chunk_name.split("_")
-        self.chunk_start_datetime: datetime = datetime.strptime(self.chunk_start_time, 
+        self.chunk_start_datetime = datetime.strptime(self.chunk_start_time, 
                                                                 DEFAULT_TIME_FORMAT)
         super().__init__(chunk_parent_path, chunk_name, extension = extension)
