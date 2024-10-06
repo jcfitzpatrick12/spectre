2aaf2ed1b0cb494ec9d0485cccba8a0006f25080
jcfitzpatrick12 <jcfitzpatrick12@gmail.com>
2024-10-06 14:21:12 +0100

    feat: basic scrap plotting script

--- client/scrap.py
+++ client/scrap.py
@@ -1,26 +1,15 @@
-import matplotlib.pyplot as plt
-import numpy as np
+from spectre.file_handlers.chunks.Chunks import Chunks
+from spectre.spectrogram.Spectrogram import Spectrogram
 
-from spectre.chunks.Chunks import Chunks
-from spectre.spectrogram.transform import (
-    join_spectrograms,
-    frequency_chop,
-    time_average,
-)
+if __name__ == "__main__":
+     chunks = Chunks("RSPduo-tuner-1-sweep-example", year=2024, month=10, day=5)
+     for chunk in chunks:
+         if chunk.has_file("fits"):
+             fits_chunk = chunk.get_file("fits")
+             spectrogram = fits_chunk.read()
+             spectrogram.quick_plot(log_norm=True)
+    
 
-chunks = Chunks("RSP1A-fixed-example", year=2024, month=9, day=11)
-
-for chunk in chunks:
-    if chunk.fits.exists():
-        S = chunk.fits.read()
-        S.quick_plot(log_norm=True)
-
-# spectrograms = [chunk.fits.read() for chunk in chunks if chunk.fits.exists()]
-# S = join_spectrograms(spectrograms)
-# print(f"Time resolution {S.time_res_seconds} [s]")
-# print(f"Frequency resolution {S.freq_res_MHz*1e3} [kHz]")
-# print(f"Frequency range {S.freq_MHz[-1] - S.freq_MHz[0]} [MHz]")
-# S.quick_plot(log_norm=True)
 
 
 
