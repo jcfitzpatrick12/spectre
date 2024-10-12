9b7f48669085a9c99f82013096e269c8a3710455
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-10-12 20:57:46 +0100

    feat: include example code

---
+++ client/examples/quick_plot.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from spectre.chunks import Chunks
+from spectre.cfg import DEFAULT_TIME_FORMAT
+
+def main(tag: str, 
+         start_time: str, 
+         end_time: str):
+    start_datetime = datetime.strptime(start_time, 
+                                       DEFAULT_TIME_FORMAT)
+    chunks = Chunks(tag, 
+                    year=start_datetime.year, 
+                    month=start_datetime.month,
+                    day=start_datetime.day)
+    spectrogram = chunks.get_spectrogram_from_range(start_time, end_time)
+    print(f"Frequency resolution {spectrogram.freq_res_MHz} [MHz]")
+    print(f"Time resolution: {spectrogram.time_res_seconds} [s]")
+    spectrogram.quick_plot(time_type = "datetimes", log_norm=True)
+
+if __name__ == "__main__":
+    tag = "rsp1a-sweep-example"
+    start_time, end_time = "2024-10-12T19:50:00", "2024-10-12T20:00:00"
+    main(tag, start_time, end_time)
+
+
+
+
+
+
+
+        
