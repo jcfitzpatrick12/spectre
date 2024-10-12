7eb4f7799319abebdfe691a1007b2d11ce601b83
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-10-12 21:02:31 +0100

    chore: change units MHz -> Hz

--- client/examples/quick_plot.py
+++ client/examples/quick_plot.py
@@ -13,13 +13,13 @@ def main(tag: str,
                     month=start_datetime.month,
                     day=start_datetime.day)
     spectrogram = chunks.get_spectrogram_from_range(start_time, end_time)
-    print(f"Frequency resolution {spectrogram.freq_res_MHz} [MHz]")
+    print(f"Frequency resolution {spectrogram.freq_res_MHz * 1e6} [Hz]")
     print(f"Time resolution: {spectrogram.time_res_seconds} [s]")
     spectrogram.quick_plot(time_type = "datetimes", log_norm=True)
 
 if __name__ == "__main__":
     tag = "rsp1a-sweep-example"
-    start_time, end_time = "2024-10-12T19:50:00", "2024-10-12T20:00:00"
+    start_time, end_time = "2024-10-12T20:00:00", "2024-10-12T20:10:00"
     main(tag, start_time, end_time)
 
 
