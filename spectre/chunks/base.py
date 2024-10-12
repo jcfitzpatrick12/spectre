48e04420ce9375348118058f37cd8a7799631f62
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-10-12 15:00:33 +0100

    chore: cosmetic formatting

--- spectre/chunks/base.py
+++ spectre/chunks/base.py
@@ -18,7 +18,7 @@ class ChunkFile(BaseFileHandler):
         self.extension = extension
         self.chunk_start_time, self.tag = chunk_name.split("_")
         self.chunk_start_datetime = datetime.strptime(self.chunk_start_time, 
-                                                                DEFAULT_TIME_FORMAT)
+                                                      DEFAULT_TIME_FORMAT)
         super().__init__(chunk_parent_path, chunk_name, extension = extension)
 
 
