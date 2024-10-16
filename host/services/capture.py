d6e9f5dcf20fe5d849a0cdcbbfa5f67af18250f2
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-10-16 23:25:17 +0100

    fix: use a different logger class instance

--- host/services/capture.py
+++ host/services/capture.py
@@ -87,6 +87,7 @@ def _start_capture(receiver_name: str,
                    mode: str, 
                    tags: List[str]) -> None:
     configure_root_logger("WORKER") #  start worker log
+    _LOGGER = multiprocessing.get_logger() # get a threadsafe multiprocessing logger
     _LOGGER.info(f"Starting capture with the receiver: {receiver_name} operating in mode: {mode} with tags: {tags}")
     receiver = get_receiver(receiver_name, mode=mode)
     receiver.start_capture(tags)
@@ -94,6 +95,7 @@ def _start_capture(receiver_name: str,
 
 def _start_watcher(tags: List[str]) -> None:
     configure_root_logger("WORKER") #  start worker log
+    _LOGGER = multiprocessing.get_logger() # get a threadsafe multiprocessing logger
     _LOGGER.info(f"Starting watcher with tags {tags}")
     for tag in tags:
         watcher = Watcher(tag)
