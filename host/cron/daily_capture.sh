f4885a44a38c6fd92b5cf8634c728e6543093ad1
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-10-19 00:38:34 +0100

    feat: incorporate logging in daily capture

--- host/cron/daily_capture.sh
+++ host/cron/daily_capture.sh
@@ -10,10 +10,8 @@ export PYTHONPATH="${SPECTRE_DIR_PATH}:${PYTHONPATH}"
 # ensure that spectre is recognised as a command in the cron environment
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-# clear the daily capture log
-cat /dev/null > /var/spectre/daily_capture.log
 # start the capture session
-spectre capture session --receiver RSP1A --mode sweep --tag RSP1A-sweeper --hours 8 --force-restart
+spectre --log capture session --receiver RSP1A --mode sweep --tag RSP1A-sweeper --hours 8 --force-restart
 # deletes all remnant bin and hdr chunk files in all chunks subdirectories
-spectre delete chunks --tag RSP1A-sweeper --extension bin --extension hdr --suppress-doublecheck
+spectre --log delete chunks --tag RSP1A-sweeper --extension bin --extension hdr --suppress-doublecheck
 
