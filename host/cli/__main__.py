6b933ed3185fcff3bc84006d8b2990f4fdc4a3ef
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-10-25 22:03:21 +0100

    chore: remove changes

--- host/cli/__main__.py
+++ host/cli/__main__.py
@@ -8,14 +8,4 @@ def main():
     spectre_cli.app(prog_name=__app_name__)
 
 if __name__ == "__main__":
-    import cProfile
-    import pstats
-
-    # Profile and store the results in a file
-    with cProfile.Profile() as pr:
-        main()
-    
-    # Create Stats object and sort by cumulative time
-    stats = pstats.Stats(pr)
-    stats.sort_stats("cumulative").print_stats(20)  # Prints top 20 slowest functions
-    # main()
+    main()
