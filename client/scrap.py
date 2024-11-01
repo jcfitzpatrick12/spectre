3dae0b5ac4d334c6712318fdb5fdbc2fe436cd0c
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-11-01 12:58:46 +0000

    chore: remove old file

--- client/scrap.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-from spectre.chunks import Chunks
-from spectre.logging import configure_root_logger
-from spectre.receivers.factory import get_receiver
-
-configure_root_logger("USER", logging.INFO)
-test = get_receiver("test")
