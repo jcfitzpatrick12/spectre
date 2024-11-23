9bdf32aa608d2351823e0c2f4974ffe4ddc73d97
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-11-23 12:51:50 +0000

    feat: initial (incomplete) commit for test cmd

---
+++ host/cli/tools/test.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: © 2024 Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
+# This file is part of SPECTRE
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import typer
+from typing import List
+
+from host.services import test
+from host.cli import (
+    TAGS_HELP,
+    SECONDS_HELP,
+    MINUTES_HELP,
+    HOURS_HELP
+)
+
+app = typer.Typer()
+
+
+@app.command()
+def session(tags: List[str] = typer.Option(..., "--tag", "-t", help=TAGS_HELP),
+            seconds: int = typer.Option(0, "--seconds", help=SECONDS_HELP),
+            minutes: int = typer.Option(0, "--minutes", help=MINUTES_HELP),
+            hours: int = typer.Option(0, "--hours", help=HOURS_HELP)
+) -> None:
+    test.end_to_end(tags,
+                    seconds = seconds,
+                    minutes = minutes,
+                    hours = hours)
+    raise typer.Exit()
---
+++ host/services/test.py
@@ -0,0 +1,49 @@
+# SPDX-FileCopyrightText: © 2024 Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
+# This file is part of SPECTRE
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from logging import getLogger
+_LOGGER =  getLogger(__name__)
+
+from host.services import capture
+
+from spectre.receivers.factory import get_receiver
+from spectre.logging import log_service_call
+from spectre.file_handlers.json_configs import CaptureConfigHandler
+
+@log_service_call(_LOGGER)
+def end_to_end(
+    tags: str,
+    seconds: int = 0,
+    minutes: int = 0,
+    hours: int = 0,
+) -> None:
+    """Do an end-to-end using the test receiver.
+    
+    Generates spectrograms for a time defined by the user.
+    For all those created, compares to the corresponding 
+    analytically derived solution.
+    """
+
+    num_tags = len(tags)
+    if num_tags != 1:
+        raise ValueError(f"Expected one tag! Got {num_tags}")
+    
+    capture_config_handler = CaptureConfigHandler(tags[0])
+    capture_config = capture_config_handler.read()
+
+    receiver_name = capture_config['receiver']
+    if receiver_name != "test":
+        raise ValueError((f"Tag must correspond to a test receiver. "
+                          f"Found receiver {receiver_name} in the capture config for tag {tag}"))
+
+    mode = capture_config['mode']
+
+    # generate spectrograms 
+    capture.session(receiver_name,
+                    mode,
+                    tags,
+                    seconds = seconds,
+                    minutes = minutes,
+                    hours = hours)
+    return
