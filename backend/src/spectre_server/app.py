33e3e823245d27e4563ec0c101b7103d96bc58d5
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-11-29 18:19:34 +0000

    fix: change host IP address inside container

--- backend/src/spectre_server/app.py
+++ backend/src/spectre_server/app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 app.register_blueprint(capture_blueprint, url_prefix="/capture")
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=5000)
