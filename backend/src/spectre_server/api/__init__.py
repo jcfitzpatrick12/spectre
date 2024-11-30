4296e45f6028362f321d684e7fda8f41447921fc
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-11-30 01:00:50 +0000

    chore: simplify logic

--- backend/src/spectre_server/api/__init__.py
+++ backend/src/spectre_server/api/__init__.py
@@ -2,52 +2,48 @@
 # This file is part of SPECTRE
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from typing import Optional, Callable
+from typing import Optional
+import os
 
-import logging
-
-from flask import jsonify, request
+from flask import jsonify
 from http import HTTPStatus
 
-from spectre_core.logging import configure_root_logger
-
 ALLOWED_STATUSES = {"success", "fail", "error"}
-DATA_REQUIRED_STATUSES = {"success", "fail"}
-MESSAGE_REQUIRED_STATUSES = {"error"}
-DEFAULT_LOG_LEVEL = logging.info
 
 def jsend_response(
-    status: str,
+    status: str, 
     data: Optional[dict] = None,
     message: Optional[str] = None,
     code: Optional[HTTPStatus] = None):
-    """All API responses adhere to https://github.com/omniti-labs/jsend"""
-    if status not in ALLOWED_STATUSES:
-        raise ValueError(f"Invalid status. Expected one of {ALLOWED_STATUSES}, got '{status}'.")
+    """Create a standardised Jsend API response.
+    
+    Adheres to https://github.com/omniti-labs/jsend"""
+    # avoid mutable default
+    if data is None:
+        data = {}
 
-    if status in MESSAGE_REQUIRED_STATUSES and message is None:
-        raise ValueError(f"Message is required for status '{status}', but got None.")
+    if status not in ALLOWED_STATUSES:
+        raise ValueError(f"Invalid status. Expected one of {ALLOWED_STATUSES}, but got: {status}")
 
     response = {"status": status}
 
-    if status in DATA_REQUIRED_STATUSES:
-        # if data is required, even if it is missing, set to null
+    if status in ["fail", "success"]:
+        # add the current pid (so user can find corresponding logs)
+        data["pid"] = os.getpid()
         response["data"] = data
+        return jsonify(response)
 
-    if status == "error":
+    elif status == "error":
+        if message is None:
+            raise ValueError(f"Message is required for status {status}, but got: {message}")
         response["message"] = message
+
+        if data:
+            response["data"] = data 
         if code is not None:
             response["code"] = code
-        if data is not None:
-            response["data"] = data 
 
-    return jsonify(response)
-
-
-def configure_logging(func: Callable):
-    def wrapper(*args, **kwargs):
-        data = request.get_json()
-        data.get("log_level")
-        configure_root_logger("USER", data.get("log_level", 10))
-        return func(*args, **kwargs)
-    return wrapper
+        return jsonify(response)
+    
+    else:
+        raise ValueError(f"Invalid status. Expected one of {ALLOWED_STATUSES}, but got: {status}")
