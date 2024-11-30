5ae77ce795c1ab8502ebfc900de40c4c3f7dc429
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-11-30 21:42:36 +0000

    feat: enable easier development inside the container

---
+++ backend/scripts/installation/spectre_backend.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: © 2024 Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
+# This file is part of SPECTRE
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# cloning enables backend development inside the container
+# sparse-checkout means we only take what's necessary from the repo
+
+# don't checkout HEAD when clone is complete
+git clone --no-checkout https://github.com/jcfitzpatrick12/spectre.git
+cd spectre
+
+# --cone means all paths inside specified directories are included
+git sparse-checkout init
+# Update the working directory only with the backend folder
+git sparse-checkout set backend/
+# Update the working directory
+git checkout formal-installation
+
+# install dependencies (in editable mode)
+cd backend
+pip install --upgrade pip
+pip install -e .
