bbe44b6e4c6355d7e99e64700de6f5af9f9e258a
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-12-03 15:16:49 +0000

    chore: move help constants

--- src/spectre_cli/__init__.py
+++ src/spectre_cli/__init__.py
@@ -2,31 +2,6 @@
 # This file is part of SPECTRE
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from spectre_core.logging import PROCESS_TYPES
-
 APP_NAME = "spectre"
 VERSION = "0.0.1"
-
 BASE_URL = "http://127.0.0.1:5000"
-
-RECEIVER_NAME_HELP = "The name of the receiver"
-MODE_HELP = "The operating mode for the receiver"
-TAG_HELP = "The capture config tag"
-SECONDS_HELP = "Seconds component of the session duration"
-MINUTES_HELP = "Minutes component of the session duration"
-HOURS_HELP = "Hours component of the session duration"
-DAY_HELP = "Day of the month (numeric)"
-MONTH_HELP = "Month of the year (numeric)"
-YEAR_HELP = "Year (numeric)"
-FORCE_RESTART_HELP = "If a worker process stops unexpectedly, terminate all subprocesses and restart the capture session"
-FORCE_UPDATE_HELP = "Force configuration files update if chunks exist under the given tag"
-PARAMS_HELP = "Pass arbitrary key-value pairs"
-PROCESS_TYPE_HELP = f"Processes may be one of {PROCESS_TYPES}"
-EXTENSIONS_HELP = "File extensions"
-SUPPRESS_DOUBLECHECK_HELP = "Suppress doublecheck prompts on file operations"
-PID_HELP = "Process ID"
-FILE_NAME_HELP = "File name (specified without extension)"
-AS_COMMAND_HELP = "Output is expressed as a runnable command"
-INSTRUMENT_CODE_HELP = "The case-sensitive CALLISTO instrument code"
-ABSOLUTE_TOLERANCE_HELP = "The value of the 'atol' keyword argument for np.isclose"
-PER_SPECTRUM_HELP = "Show validated status per spectrum."
--- src/spectre_cli/commands/__init__.py
+++ src/spectre_cli/commands/__init__.py
@@ -6,6 +6,29 @@ from typing import Callable
 from functools import wraps
 
 import typer
+from spectre_core.logging import PROCESS_TYPES
+
+RECEIVER_NAME_HELP = "The name of the receiver"
+MODE_HELP = "The operating mode for the receiver"
+TAG_HELP = "The capture config tag"
+SECONDS_HELP = "Seconds component of the session duration"
+MINUTES_HELP = "Minutes component of the session duration"
+HOURS_HELP = "Hours component of the session duration"
+DAY_HELP = "Day of the month (numeric)"
+MONTH_HELP = "Month of the year (numeric)"
+YEAR_HELP = "Year (numeric)"
+FORCE_RESTART_HELP = "If a worker process stops unexpectedly, terminate all subprocesses and restart the capture session"
+FORCE_UPDATE_HELP = "Force configuration files update if chunks exist under the given tag"
+PARAMS_HELP = "Pass arbitrary key-value pairs"
+PROCESS_TYPE_HELP = f"Processes may be one of {PROCESS_TYPES}"
+EXTENSIONS_HELP = "File extensions"
+SUPPRESS_DOUBLECHECK_HELP = "Suppress doublecheck prompts on file operations"
+PID_HELP = "Process ID"
+FILE_NAME_HELP = "File name (specified without extension)"
+AS_COMMAND_HELP = "Output is expressed as a runnable command"
+INSTRUMENT_CODE_HELP = "The case-sensitive CALLISTO instrument code"
+ABSOLUTE_TOLERANCE_HELP = "The value of the 'atol' keyword argument for np.isclose"
+PER_SPECTRUM_HELP = "Show validated status per spectrum."
 
 def secho_response(func: Callable):
     """Print the jsendified response from the spectre server"""
@@ -17,3 +40,4 @@ def secho_response(func: Callable):
         elif jsend_response['status'] == "error":
             typer.secho(jsend_response["message"], fg="yellow")
     return wrapper
+
