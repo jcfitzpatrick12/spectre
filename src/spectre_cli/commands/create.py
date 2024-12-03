a919b0bed7992807c010731bcd82f728504a5d4c
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-12-03 21:37:26 +0000

    feat: add CLI entrypoint for create functionality

--- src/spectre_cli/commands/create.py
+++ src/spectre_cli/commands/create.py
@@ -4,39 +4,54 @@
 
 import typer
 from typing import List
+import requests
 
+from spectre_cli import BASE_URL
+from spectre_cli.commands import secho_response
 from spectre_cli.commands import (
     RECEIVER_NAME_HELP,
     MODE_HELP,
     TAG_HELP,
     PARAMS_HELP,
+    FORCE_HELP
 )
 
 app = typer.Typer()
 
 @app.command()
+@secho_response
 def fits_config(tag: str = typer.Option(..., "--tag", "-t", help=TAG_HELP),
-                params: List[str] = typer.Option([], "--param", "-p", help=PARAMS_HELP, metavar="KEY=VALUE")
+                params: List[str] = typer.Option([], "--param", "-p", help=PARAMS_HELP, metavar="KEY=VALUE",),
+                force: bool = typer.Option(False, "--force", help = FORCE_HELP, is_flag=True)
 ) -> None:
     
-    # create.capture_config(tag,
-    #                       params)
-    typer.secho(f"The fits-config for tag \"{tag}\" has been successfully created", fg=typer.colors.GREEN)
-    raise typer.Exit()
+    payload = {
+        "tag": tag,
+        "params": params,
+        "force": force
+    }
+    return requests.post(f"{BASE_URL}/create/fits-config",
+                         json = payload)
+
 
 @app.command()
+@secho_response
 def capture_config(tag: str = typer.Option(..., "--tag", "-t", help=TAG_HELP),
                    receiver_name: str = typer.Option(..., "--receiver", "-r", help=RECEIVER_NAME_HELP),
                    mode: str = typer.Option(..., "--mode", "-m", help=MODE_HELP),
                    params: List[str] = typer.Option([], "--param", "-p", help=PARAMS_HELP, metavar="KEY=VALUE"),
+                   force: bool = typer.Option(False, "--force", help = FORCE_HELP, is_flag=True)
 ) -> None:
     
-    # create.capture_config(tag,
-    #                       receiver_name,
-    #                       mode,
-    #                       params)
-    typer.secho(f"The capture-config for tag \"{tag}\" has been successfully created", fg=typer.colors.GREEN)
-    raise typer.Exit()
+    payload = {
+        "tag": tag,
+        "receiver_name": receiver_name,
+        "mode": mode,
+        "params": params,
+        "force": force
+    }
+    return requests.post(f"{BASE_URL}/create/fits-config",
+                         json = payload)
 
         
 
