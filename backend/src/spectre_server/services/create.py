b988bce3297627e4fb75ac748d4ce4d1f9dc6051
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-12-04 20:48:00 +0000

    feat: print file name from config creation

--- backend/src/spectre_server/services/create.py
+++ backend/src/spectre_server/services/create.py
@@ -8,7 +8,7 @@ _LOGGER = getLogger(__name__)
 from typing import List, Optional
 
 from spectre_core.receivers.factory import get_receiver
-from spectre_core.file_handlers.configs import FitsConfig
+from spectre_core.file_handlers.configs import FitsConfig, CaptureConfig
 from spectre_core.logging import log_call
 
 
@@ -16,14 +16,18 @@ from spectre_core.logging import log_call
 def fits_config(tag: str,
                 params: Optional[List[str]] = None,
                 force: bool = False,
-) -> None:
+) -> dict[str, str]:
+    """Create a fits-config, and return its file name."""
+
     if params is None:
         params = []
+
     fits_config = FitsConfig(tag)
     fits_config.save_params(params,
                             force)
-    _LOGGER.info(f"The fits-config for tag: {tag} has been created")
+    _LOGGER.info(f"The fits-config for tag '{tag}' has been created: {fits_config.file_name}")
 
+    return {"file_name": fits_config.file_name}
 
 @log_call
 def capture_config(tag: str,
@@ -31,12 +35,18 @@ def capture_config(tag: str,
                    mode: str,
                    params: Optional[List[str]],
                    force: bool = False
-) -> None:
+) -> str:
+    """Create a capture config and return its file name."""
     if params is None:
         params = []
+        
     receiver = get_receiver(receiver_name, 
                             mode = mode)
     receiver.save_params(params,
                          tag,
                          force)
-    _LOGGER.info(f"The capture-config for tag: {tag} has been created")
+    _LOGGER.info(f"The capture-config for tag '{tag}' has been created: {capture_config.file_name}")
+
+    # create an instance of the newly created capture config
+    capture_config = CaptureConfig(tag)
+    return {"file_name": capture_config.file_name}
