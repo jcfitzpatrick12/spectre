6228b2404f12ad5abfa272e0bd98dedcc309a063
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-12-05 12:06:42 +0000

    feat: pretty print test results

--- src/spectre_cli/commands/test.py
+++ src/spectre_cli/commands/test.py
@@ -3,7 +3,9 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import typer
+from spectre_core.spectrograms.analytical import TestResults
 
+from spectre_cli.commands import safe_request
 from spectre_cli.commands import (
     TAG_HELP,
     ABSOLUTE_TOLERANCE_HELP,
@@ -13,27 +15,28 @@ from spectre_cli.commands import (
 test_app = typer.Typer()
 
 
-# def _pretty_print_test_results(file_name: str, 
-#                                test_results: test.TestResults, 
-#                                per_spectrum: bool) -> None:
-#     """Print test results with appropriate formatting and colours."""
+def _pretty_print_test_results(file_name: str, 
+                               test_results: TestResults, 
+                               per_spectrum: bool
+) -> None:
+    """Print test results with appropriate formatting and colours."""
     
-#     def print_colored(label: str, value: bool) -> None:
-#         typer.secho(f"{label}: {'PASS' if value else 'FAIL'}", fg="green" if value else "red")
+    def print_colored(label: str, value: bool) -> None:
+        typer.secho(f"{label}: {'PASS' if value else 'FAIL'}", fg="green" if value else "red")
 
-#     def print_spectrum_results():
-#         typer.secho("\nPer spectrum results:" if per_spectrum else "\nSummary:")
-#         if per_spectrum:
-#             for time, is_valid in test_results.spectrum_validated.items():
-#                 typer.secho(f"  Time {time:.3f} [s]: {'PASS' if is_valid else 'FAIL'}", fg="green" if is_valid else "red")
-#         else:
-#             typer.secho(f"  Validated spectrums: {test_results.num_validated_spectrums}", fg="green")
-#             typer.secho(f"  Invalid spectrums: {test_results.num_invalid_spectrums}", fg="red")
+    def print_spectrum_results():
+        typer.secho("\nPer spectrum results:" if per_spectrum else "\nSummary:")
+        if per_spectrum:
+            for time, is_valid in test_results.spectrum_validated.items():
+                typer.secho(f"  Time {time:.3f} [s]: {'PASS' if is_valid else 'FAIL'}", fg="green" if is_valid else "red")
+        else:
+            typer.secho(f"  Validated spectrums: {test_results.num_validated_spectrums}", fg="green")
+            typer.secho(f"  Invalid spectrums: {test_results.num_invalid_spectrums}", fg="red")
 
-#     typer.secho(f"\nTest results for {file_name}:", bold=True)
-#     print_colored("Times validated", test_results.times_validated)
-#     print_colored("Frequencies validated", test_results.frequencies_validated)
-#     print_spectrum_results()
+    typer.secho(f"\nTest results for {file_name}:", bold=True)
+    print_colored("Times validated", test_results.times_validated)
+    print_colored("Frequencies validated", test_results.frequencies_validated)
+    print_spectrum_results()
 
 
 @test_app.command()
@@ -42,10 +45,20 @@ def analytical(
     absolute_tolerance: float = typer.Option(1e-3, "--atol", "--absolute-tolerance", help=ABSOLUTE_TOLERANCE_HELP),
     per_spectrum: bool = typer.Option(False, "--per-spectrum", help=PER_SPECTRUM_HELP),
 ) -> None:
-    # results_per_chunk = test.analytical(tag, absolute_tolerance)
+    payload = {
+        "tag": tag,
+        "absolute_tolerance": absolute_tolerance,
+    }
+    jsend_dict = safe_request("test/analytical",
+                              "GET",
+                              payload)
+    results_per_chunk = jsend_dict["data"]
+    for file_name, test_results in results_per_chunk.items():
+        test_results = TestResults(test_results["times_validated"],
+                                   test_results["frequencies_validated"],
+                                   test_results["spectrum_validated"])
 
-    # for file_name, test_results in results_per_chunk.items():
-    #     _pretty_print_test_results(file_name, 
-    #                                test_results, 
-    #                                per_spectrum)
+        _pretty_print_test_results(file_name,
+                                   test_results,
+                                   per_spectrum)
     raise typer.Exit()
