b733bafb3d6f6b6acd2282160f2f1e10c8209b86
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-12-05 12:43:37 +0000

    feat: add update routes

---
+++ backend/src/spectre_server/routes/update.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: © 2024 Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
+# This file is part of SPECTRE
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from flask import Blueprint, request
+
+from spectre_server.services import update
+from spectre_server.routes import jsendify_response
+
+
+update_blueprint = Blueprint("update", __name__)
+
+@update_blueprint.route("capture-config", methods=["PATCH"])
+@jsendify_response
+def capture_config():
+    payload = request.get_json()
+    tag = payload.get("tag")
+    params = payload.get("params")
+    force = payload.get("force")
+    return update.capture_config(tag,
+                                 params,
+                                 force)
+
+
+@update_blueprint.route("fits-config", methods=["PATCH"])
+@jsendify_response
+def fits_config():
+    payload = request.get_json()
+    tag = payload.get("tag")
+    params = payload.get("params")
+    force = payload.get("force")
+    return update.fits_config(tag,
+                              params,
+                              force)
