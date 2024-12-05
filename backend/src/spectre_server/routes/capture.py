87dc2302bfdc91f2b5e27ec72f7d41c78ba268aa
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-12-05 00:49:13 +0000

    chore: whitespace formatting

--- backend/src/spectre_server/routes/capture.py
+++ backend/src/spectre_server/routes/capture.py
@@ -2,11 +2,11 @@
 # This file is part of SPECTRE
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-
 from flask import Blueprint, request
 
 from spectre_server.services import capture
 from spectre_server.routes import jsendify_response
+
 
 capture_blueprint = Blueprint("capture", __name__)
 
