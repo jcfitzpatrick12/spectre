33a45635c06a6d256ce0ea4c741ac1b050f03e9d
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-12-05 13:40:18 +0000

    feat: add web-fetch blueprint

--- backend/src/spectre_server/main.py
+++ backend/src/spectre_server/main.py
@@ -7,6 +7,7 @@ from spectre_server.routes.delete import delete_blueprint
 from spectre_server.routes.get import get_blueprint
 from spectre_server.routes.test import test_blueprint
 from spectre_server.routes.update import update_blueprint
+from spectre_server.routes.web_fetch import web_fetch_blueprint
 
 app = Flask(__name__)
 
@@ -17,6 +18,7 @@ app.register_blueprint(delete_blueprint, url_prefix="/delete")
 app.register_blueprint(get_blueprint, url_prefix="/get")
 app.register_blueprint(test_blueprint, url_prefix="/test")
 app.register_blueprint(update_blueprint, url_prefix="/update")
+app.register_blueprint(web_fetch_blueprint, url_prefix="/web-fetch")
 
 
 if __name__ == "__main__":
---
+++ backend/src/spectre_server/routes/web_fetch.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: © 2024 Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
+# This file is part of SPECTRE
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from flask import Blueprint, request
+
+from spectre_server.services import web_fetch
+from spectre_server.routes import jsendify_response
+
+web_fetch_blueprint = Blueprint("web-fetch", __name__)
+
+@web_fetch_blueprint.route("/callisto")
+@jsendify_response
+def callisto():
+    payload = request.get_json()
+    instrument_code = payload.get("instrument_code")
+    year = payload.get("year")
+    month = payload.get("month")
+    day = payload.get("day")
+    web_fetch.callisto(instrument_code,
+                       year,
+                       month,
+                       day)
