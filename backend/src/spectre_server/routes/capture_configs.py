9a325d88d6b283c9e109f7e2dc8d68b960fab747
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-12-22 22:09:07 +0000

    fix: update URL paths

--- backend/src/spectre_server/routes/capture_configs.py
+++ backend/src/spectre_server/routes/capture_configs.py
@@ -4,28 +4,28 @@
 
 from flask import Blueprint, request
 
-from spectre_server.services import configs
+from spectre_server.services import capture_configs
 from spectre_server.routes import jsendify_response
 
 
-configs_blueprint = Blueprint("capture-configs", __name__)
+capture_configs_blueprint = Blueprint("capture-configs", __name__)
 
 
-@configs_blueprint.route("", methods=["GET"])
+@capture_configs_blueprint.route("", methods=["GET"])
 @jsendify_response
 def get_capture_configs(
 ):
-    return configs.get_capture_configs()
+    return capture_configs.get_capture_configs()
 
 
-@configs_blueprint.route("/<string:tag>", methods=["GET"])
+@capture_configs_blueprint.route("/<string:tag>", methods=["GET"])
 @jsendify_response
 def get_capture_config(tag: str
 ):
-    return configs.get_capture_config(tag)
+    return capture_configs.get_capture_config(tag)
 
 
-@configs_blueprint.route("/<string:tag>", methods=["PUT"])
+@capture_configs_blueprint.route("/<string:tag>", methods=["PUT"])
 @jsendify_response
 def create_capture_config(tag: str
 ):
@@ -34,27 +34,27 @@ def create_capture_config(tag: str
     mode              = json.get("mode")
     string_parameters = json.get("string_parameters")
     force             = json.get("force")
-    return configs.create_capture_config(tag, 
+    return capture_configs.create_capture_config(tag, 
                                          receiver_name,
                                          mode,
                                          string_parameters,
                                          force)
 
 
-@configs_blueprint.route("/<string:tag>", methods=["DELETE"])
+@capture_configs_blueprint.route("/<string:tag>", methods=["DELETE"])
 @jsendify_response
 def delete_capture_config(tag: str
 ):
-    return configs.delete_capture_config(tag)
+    return capture_configs.delete_capture_config(tag)
 
 
-@configs_blueprint.route("/<string:tag>", methods=["PATCH"])
+@capture_configs_blueprint.route("/<string:tag>", methods=["PATCH"])
 @jsendify_response
 def update_capture_config(tag: str
 ):
     json              = request.get_json()
     string_parameters = json.get("string_parameters")
     force             = json.get("force")
-    return configs.update_capture_config(tag, 
+    return capture_configs.update_capture_config(tag, 
                                          string_parameters,
                                          force)
