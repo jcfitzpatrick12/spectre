af968359c4ab998a63aca4ff5309b6de3d0adcf5
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-12-30 20:31:40 +0000

    feat: initial commit following nomenclature change

--- backend/src/spectre_server/routes/batches.py
+++ backend/src/spectre_server/routes/batches.py
@@ -5,70 +5,70 @@
 
 from flask import Blueprint, request
 
-from spectre_server.services import chunks
+from spectre_server.services import batches
 from spectre_server.routes import jsendify_response
 
 
-chunks_blueprint = Blueprint("chunks", __name__)
+batches_blueprint = Blueprint("batches", __name__)
 
 
-@chunks_blueprint.route("", methods=["GET"])
+@batches_blueprint.route("", methods=["GET"])
 @jsendify_response
-def get_chunk_files():
+def get_batch_files():
     tags       = request.args.getlist("tag")
     extensions = request.args.getlist("extension")
     year       = request.args.get("year" , type = int)
     month      = request.args.get("month", type = int)
     day        = request.args.get("day"  , type = int)
-    return chunks.get_chunk_files(tags,
+    return batches.get_batch_files(tags,
                                   extensions, 
                                   year,
                                   month,
                                   day)
 
 
-@chunks_blueprint.route("/<string:tag>", methods=["GET"])
+@batches_blueprint.route("/<string:tag>", methods=["GET"])
 @jsendify_response
-def get_chunk_files_for_tag(tag: str):
+def get_batch_files_for_tag(tag: str):
     extensions = request.args.getlist("extension")
     year       = request.args.get("year" , type = int)
     month      = request.args.get("month", type = int)
     day        = request.args.get("day"  , type = int)
-    return chunks.get_chunk_files_for_tag(tag,
+    return batches.get_batch_files_for_tag(tag,
                                           extensions, 
                                           year,
                                           month,
                                           day)
 
 
-@chunks_blueprint.route("/<string:tag>", methods=["DELETE"])
+@batches_blueprint.route("/<string:tag>", methods=["DELETE"])
 @jsendify_response
-def delete_chunk_files(tag: str):
+def delete_batch_files(tag: str):
     extensions = request.args.getlist("extension")
     year  = request.args.get("year" , type = int)
     month = request.args.get("month", type = int)
     day   = request.args.get("day"  , type = int)
-    return chunks.delete_chunk_files(tag,
+    return batches.delete_batch_files(tag,
                                      extensions, 
                                      year,
                                      month,
                                      day)
 
 
-@chunks_blueprint.route("/<string:tag>/analytical-test-results", methods=["GET"])
+@batches_blueprint.route("/<string:tag>/analytical-test-results", methods=["GET"])
 @jsendify_response
 def get_analytical_test_results(tag: str):
     absolute_tolerance = request.args.get("absolute_tolerance", type = float)
-    return chunks.get_analytical_test_results(tag,
+    return batches.get_analytical_test_results(tag,
                                               absolute_tolerance)
 
 
-@chunks_blueprint.route("/tags", methods=["GET"])
+@batches_blueprint.route("/tags", methods=["GET"])
 @jsendify_response
 def get_tags():
     year  = request.args.get("year" , type = int)
     month = request.args.get("month", type = int)
     day   = request.args.get("day"  , type = int)
-    return chunks.get_tags(year,
+    return batches.get_tags(year,
                            month,
                            day)
