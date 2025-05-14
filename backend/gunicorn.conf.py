7262b508d82438edb06dce317d5603f09282de34
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2025-05-15 00:03:19 +0100

    fix: Define multiple `gunicorn` workers

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -3,9 +3,10 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import os
+import multiprocessing
 
 SPECTRE_SERVICE_HOST = os.environ.get("SPECTRE_SERVICE_HOST", "0.0.0.0")
 SPECTRE_SERVICE_PORT = int(os.environ.get("SPECTRE_SERVICE_PORT", "5000"))
 
 bind = f"{SPECTRE_SERVICE_HOST}:{SPECTRE_SERVICE_PORT}"
-
+workers = multiprocessing.cpu_count() * 2 + 1 
