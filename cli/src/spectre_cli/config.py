0374d55040f8d088b429495d672ea5c23e1e8f46
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2025-05-17 12:31:24 +0100

    chore: Remove whitespace

--- cli/src/spectre_cli/config.py
+++ cli/src/spectre_cli/config.py
@@ -8,6 +8,6 @@ SPECTRE_SERVER_PORT = os.environ.get("SPECTRE_SERVER_PORT", None)
 if SPECTRE_SERVER_HOST is not None and SPECTRE_SERVER_PORT is not None:
     SPECTRE_SERVER = f"http://{SPECTRE_SERVER_HOST}:{SPECTRE_SERVER_PORT}"
 else:
-    SPECTRE_SERVER       = os.environ.get("SPECTRE_SERVER", "http://localhost:5000")
+    SPECTRE_SERVER = os.environ.get("SPECTRE_SERVER", "http://localhost:5000")
 
 
