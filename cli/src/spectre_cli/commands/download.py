b99942ccedeff7e1315a1f08179fc9099aa1a1af
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2025-05-17 12:54:31 +0100

    chore: update copyright year to 2025

--- cli/src/spectre_cli/commands/download.py
+++ cli/src/spectre_cli/commands/download.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2024 Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
+# SPDX-FileCopyrightText: © 2024-2025 Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
 # This file is part of SPECTRE
 # SPDX-License-Identifier: GPL-3.0-or-
 
