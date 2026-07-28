41baeecbe5feeb0f3bf033bbd34dbc7a7ebd15f7
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2026-07-28 21:20:54 +0100

    chore: issue-263 Automate version bumping

--- backend/src/spectre_server/__init__.py
+++ backend/src/spectre_server/__init__.py
@@ -2,6 +2,6 @@
 # This file is part of SPECTRE
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-__version__ = "4.2.0-alpha"
+__version__ = "4.2.0"  # x-release-please-version
 
 __all__ = ["core", "routes", "services"]
